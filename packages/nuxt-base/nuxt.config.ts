ed7bfd0df8a8cb96a814056243079cd6c52ef73b
Ege İliklier <ege.iliklier@eliarge.com>
2023-09-29 13:52:14 +0300

    ci: created workflow

--- packages/nuxt-base/nuxt.config.ts
+++ packages/nuxt-base/nuxt.config.ts
@@ -8,4 +8,10 @@ export default defineNuxtConfig({
     '@element-plus/nuxt',
     'nuxt-quasar-ui',
   ],
+  // quasar: {
+  //   plugins: [
+  //     'Dialog',
+  //     'Notify',
+  //   ],
+  // },
 })
