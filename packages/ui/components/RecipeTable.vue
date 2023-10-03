1898c139bad04de7f7c7b245252f78280d88a430
Alper Atmaca <alper.atmaca@eliarge.com>
2023-10-03 15:25:42 +0300

    style: lint

--- packages/ui/components/RecipeTable.vue
+++ packages/ui/components/RecipeTable.vue
@@ -1,29 +1,29 @@
 <script setup lang="ts">
 import type { TableColumnCtx } from 'element-plus'
-import { ElTable, ElTableColumn, ElButton } from 'element-plus'
-import 'element-plus/es/components/button/style'
-import 'element-plus/es/components/table/style'
-import 'element-plus/es/components/table-column/style'
+import { ElButton, ElTable, ElTableColumn } from 'element-plus'
+import 'element-plus/es/components/button/style/css'
+import 'element-plus/es/components/table/style/css'
+import 'element-plus/es/components/table-column/style/css'
 
 interface Recipe {
-  planKey: number | null;
-  recIndex: number | null;
-  recNo: number | null;
-  name: string | null;
-  reqNumber: number | null;
-  mainStep: number | null;
-  parallelStep: number | null;
-  recType: number | null;
-  chemCode: string | null;
-  materialName: string | null;
-  amount: number | null;
-  reqBatchNo: number | null;
-  reqProgNo: number | null;
-  otherUnit: number | null;
-  phaseNo: number | null;
-  phaseIndex: number | null;
-  washingName: string | null;
-  unit: number;
+  planKey: number | null
+  recIndex: number | null
+  recNo: number | null
+  name: string | null
+  reqNumber: number | null
+  mainStep: number | null
+  parallelStep: number | null
+  recType: number | null
+  chemCode: string | null
+  materialName: string | null
+  amount: number | null
+  reqBatchNo: number | null
+  reqProgNo: number | null
+  otherUnit: number | null
+  phaseNo: number | null
+  phaseIndex: number | null
+  washingName: string | null
+  unit: number
 }
 interface RecipeTableProps {
   show: boolean
@@ -41,7 +41,7 @@ interface RecipeTableProps {
     buttonText: string
   }
   groupables: {
-    key: keyof Recipe,
+    key: keyof Recipe
     index: number
   }[]
   rows: Recipe[]
@@ -53,44 +53,43 @@ interface RecipeTableProps {
   }[]
 }
 const props = defineProps<RecipeTableProps>()
+const emits = defineEmits(['fullScren'])
 interface SpanMethodProps {
   row: Recipe
   column: TableColumnCtx<Recipe>
   rowIndex: number
   columnIndex: number
 }
-const emits = defineEmits(['fullScren'])
 function objectSpanMethod({ row, rowIndex, columnIndex }: SpanMethodProps) {
   if (props.hasObjectSpanMethod) {
-  const property = props.groupables.find(prop => prop.index === columnIndex)
-  if (!property) {
-    return { rowspan: 1, colspan: 1 }
-  }
-  const prevRow = props.rows[rowIndex - 1] || {}
-  const prevGroupables = props.groupables.slice(0, columnIndex + 1)
-  let rowspan = 1
-
-  const isSameAsPrevRow = prevGroupables.every(
-    groupable => prevRow[groupable.key] === row[groupable.key],
-  )
-
-  if (isSameAsPrevRow) {
-    return { rowspan: 0, colspan: 0 }
-  }
-  for (let i = rowIndex + 1; i < props.rows.length; i++) {
-    const nextRow = props.rows[i]
-    const isEqual = prevGroupables.every(
-      groupable => nextRow[groupable.key] === row[groupable.key],
-    )
-    if (isEqual) {
-      rowspan++
-    } else {
-      break
+    const property = props.groupables.find(prop => prop.index === columnIndex)
+    if (!property) {
+      return { rowspan: 1, colspan: 1 }
     }
-  }
-  return { rowspan, colspan: 1 }
-  }
-  else return { rowspan: 1, colspan: 1}
+    const prevRow = props.rows[rowIndex - 1] || {}
+    const prevGroupables = props.groupables.slice(0, columnIndex + 1)
+    let rowspan = 1
+
+    const isSameAsPrevRow = prevGroupables.every(
+      groupable => prevRow[groupable.key] === row[groupable.key],
+    )
+
+    if (isSameAsPrevRow) {
+      return { rowspan: 0, colspan: 0 }
+    }
+    for (let i = rowIndex + 1; i < props.rows.length; i++) {
+      const nextRow = props.rows[i]
+      const isEqual = prevGroupables.every(
+        groupable => nextRow[groupable.key] === row[groupable.key],
+      )
+      if (isEqual) {
+        rowspan++
+      } else {
+        break
+      }
+    }
+    return { rowspan, colspan: 1 }
+  } else return { rowspan: 1, colspan: 1 }
 }
 function cellClass({ row, columnIndex }: SpanMethodProps) {
   if ((columnIndex === 4 || columnIndex === 5) && row.recType === 1) {
@@ -137,18 +136,18 @@ function cellClass({ row, columnIndex }: SpanMethodProps) {
             </ElButton>
           </template>
           <ElTableColumn
-            :label="props.columns[props.columns.length-1].label"
-            :prop="props.columns[props.columns.length-1].prop"
-            :align="props.columns[props.columns.length-1].align"
-            :show-overflow-tooltip="props.columns[props.columns.length-1].showOverflowTooltip"
+            :label="props.columns[props.columns.length - 1].label"
+            :prop="props.columns[props.columns.length - 1].prop"
+            :align="props.columns[props.columns.length - 1].align"
+            :show-overflow-tooltip="props.columns[props.columns.length - 1].showOverflowTooltip"
           />
         </ElTableColumn>
         <ElTableColumn
           v-else
-          :label="props.columns[props.columns.length-1].label"
-          :prop="props.columns[props.columns.length-1].prop"
-          :align="props.columns[props.columns.length-1].align"
-          :show-overflow-tooltip="props.columns[props.columns.length-1].showOverflowTooltip"
+          :label="props.columns[props.columns.length - 1].label"
+          :prop="props.columns[props.columns.length - 1].prop"
+          :align="props.columns[props.columns.length - 1].align"
+          :show-overflow-tooltip="props.columns[props.columns.length - 1].showOverflowTooltip"
         />
       </ElTableColumn>
     </ElTable>
