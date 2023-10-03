e884d2c840c378946bad2e666a51744e608de639
EkinBarisCakir <baris.cakir@eliar.com.tr>
2023-10-03 17:16:01 +0300

    feat: add log scroll area

---
+++ apps/machine-app/components/LogScrollArea.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <q-scroll-area class="h-50">
+    <div v-for="n in 100" :key="n" class="ml-2">
+      jdlasjdl
+    </div>
+  </q-scroll-area>
+</template>
+
+<style scoped>
+
+</style>
