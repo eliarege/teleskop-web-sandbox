9a57df121ea5bb2c8017ed577f209aabc115db33
Alper Atmaca <alper.atmaca@eliarge.com>
2023-10-03 17:23:53 +0300

    chore: updated recipe table

--- packages/ui/components/RecipeTable.vue
+++ packages/ui/components/RecipeTable.vue
@@ -5,25 +5,11 @@ import 'element-plus/es/components/button/style/css'
 import 'element-plus/es/components/table/style/css'
 import 'element-plus/es/components/table-column/style/css'
 
-interface Recipe {
-  planKey: number | null
-  recIndex: number | null
-  recNo: number | null
-  name: string | null
-  reqNumber: number | null
-  mainStep: number | null
-  parallelStep: number | null
-  recType: number | null
-  chemCode: string | null
-  materialName: string | null
-  amount: number | null
-  reqBatchNo: number | null
-  reqProgNo: number | null
-  otherUnit: number | null
-  phaseNo: number | null
-  phaseIndex: number | null
-  washingName: string | null
-  unit: number
+interface SpanMethodProps {
+  row: any
+  column: TableColumnCtx<any[]>
+  rowIndex: number
+  columnIndex: number
 }
 interface RecipeTableProps {
   show: boolean
@@ -31,81 +17,68 @@ interface RecipeTableProps {
   emptyText: string
   isFirst: boolean
   hasObjectSpanMethod: boolean
-  fullScreen: boolean
+  fullScreen?: boolean
   chemClass?: string
   dyeingClass?: string
-  fullScreenButtonProps: {
+  fullScreenButtonProps?: {
     plain: boolean
     color: string
     cssCls?: string
     buttonText: string
   }
-  groupables: {
-    key: keyof Recipe
-    index: number
-  }[]
-  rows: Recipe[]
+  rows: any[]
   columns: {
     label: string
     prop: string
     align: 'left' | 'right' | 'start' | 'end' | 'center'
     showOverflowTooltip: boolean
   }[]
+  groupables: {
+    key: string
+    index: number
+  }[]
+  cellClass: string
 }
 const props = defineProps<RecipeTableProps>()
-const emits = defineEmits(['fullScren'])
-interface SpanMethodProps {
-  row: Recipe
-  column: TableColumnCtx<Recipe>
-  rowIndex: number
-  columnIndex: number
-}
+defineEmits(['fullScren', 'close'])
 function objectSpanMethod({ row, rowIndex, columnIndex }: SpanMethodProps) {
-  if (props.hasObjectSpanMethod) {
-    const property = props.groupables.find(prop => prop.index === columnIndex)
-    if (!property) {
-      return { rowspan: 1, colspan: 1 }
-    }
-    const prevRow = props.rows[rowIndex - 1] || {}
-    const prevGroupables = props.groupables.slice(0, columnIndex + 1)
-    let rowspan = 1
-
-    const isSameAsPrevRow = prevGroupables.every(
-      groupable => prevRow[groupable.key] === row[groupable.key],
-    )
-
-    if (isSameAsPrevRow) {
-      return { rowspan: 0, colspan: 0 }
-    }
-    for (let i = rowIndex + 1; i < props.rows.length; i++) {
-      const nextRow = props.rows[i]
-      const isEqual = prevGroupables.every(
-        groupable => nextRow[groupable.key] === row[groupable.key],
-      )
-      if (isEqual) {
-        rowspan++
-      } else {
-        break
-      }
-    }
-    return { rowspan, colspan: 1 }
-  } else return { rowspan: 1, colspan: 1 }
-}
-function cellClass({ row, columnIndex }: SpanMethodProps) {
-  if ((columnIndex === 4 || columnIndex === 5) && row.recType === 1) {
-    return props.chemClass
+  const property = props.groupables.find(prop => prop.index === columnIndex)
+  console.log(property)
+  if (!property) {
+    return { rowspan: 1, colspan: 1 }
   }
-  return props.dyeingClass
+  const prevRow = props.rows[rowIndex - 1] || {}
+  const prevGroupables = props.groupables.slice(0, columnIndex + 1)
+  let rowspan = 1
+
+  const isSameAsPrevRow = prevGroupables.every(
+    groupable => prevRow[groupable.key] === row[groupable.key],
+  )
+  if (isSameAsPrevRow) {
+    return { rowspan: 0, colspan: 0 }
+  }
+  for (let i = rowIndex + 1; i < props.rows.length; i++) {
+    const nextRow = props.rows[i]
+    const isEqual = prevGroupables.every(
+      groupable => nextRow[groupable.key] === row[groupable.key],
+    )
+    if (isEqual) {
+      rowspan++
+    } else {
+      break
+    }
+  }
+  return { rowspan, colspan: 1 }
 }
 </script>
 
 <template>
   <div class="w-full h-full">
     <ElTable
-      :span-method="objectSpanMethod"
       :data="rows"
       :border="true"
       table-layout="fixed"
+      :span-method="objectSpanMethod"
       header-cell-class-name="whitespace-nowrap"
       :cell-class-name="cellClass"
       :row-class-name="dyeingClass"
@@ -127,13 +100,15 @@ function cellClass({ row, columnIndex }: SpanMethodProps) {
           align="center"
         >
           <template #header>
-            <ElButton
-              :plain="fullScreenButtonProps.plain"
-              :color="fullScreenButtonProps.color"
-              @click="$emit('fullScren')"
-            >
-              {{ props.fullScreenButtonProps.buttonText }}
-            </ElButton>
+            <div>
+              <ElButton
+                :plain="fullScreenButtonProps?.plain"
+                :color="fullScreenButtonProps?.color"
+                @click="$emit('fullScren')"
+              >
+                {{ fullScreenButtonProps?.buttonText }}
+              </ElButton>
+            </div>
           </template>
           <ElTableColumn
             :label="props.columns[props.columns.length - 1].label"
