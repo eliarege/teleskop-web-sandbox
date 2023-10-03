eb1fd67ec811e1d83df143e50b9f070511235141
EkinBarisCakir <baris.cakir@eliar.com.tr>
2023-10-03 15:10:06 +0300

    feat: apps/machines

---
+++ apps/machine-app/nuxt.config.ts
@@ -0,0 +1,28 @@
+import process from 'node:process'
+import i18n from '@intlify/unplugin-vue-i18n/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  devtools: { enabled: true },
+  extends: ['nuxt-base'],
+  quasar: {
+    plugins: [
+      'Dialog',
+    ],
+  },
+  runtimeConfig: {
+    teleskopHost: process.env.TELESKOP_HOST,
+    teleskopUser: process.env.TELESKOP_USER,
+    teleskopPort: process.env.TELESKOP_PORT,
+    teleskopPassword: process.env.TELESKOP_PASSWORD,
+    teleskopDatabase: process.env.TELESKOP_DATABASE,
+  },
+  vite: {
+    plugins: [
+      i18n({
+        include: ['locales/*'],
+      }),
+    ],
+  },
+})
