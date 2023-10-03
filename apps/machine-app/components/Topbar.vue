607f5bd88d8680d807e1bcea0d2f5097326c3f6f
EkinBarisCakir <baris.cakir@eliar.com.tr>
2023-10-03 17:15:33 +0300

    feat: add topbar

---
+++ apps/machine-app/components/Topbar.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="topbar">
+    <q-btn label="Makine" flat>
+      <q-menu>
+        <q-list>
+          <q-item>
+            <q-item-section>
+              yeni
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              özellikler
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section>
+              sil
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              yükle
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              çıkış
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-menu>
+    </q-btn>
+    <q-btn label="makine grubu" flat>
+      <q-menu>
+        <q-list>
+          <q-item>
+            <q-item-selection>
+              değiştir
+            </q-item-selection>
+          </q-item>
+        </q-list>
+      </q-menu>
+    </q-btn>
+    <q-btn label="ayarlar" flat>
+      <q-menu>
+        <q-list>
+          <q-item>
+            <q-item-selection>
+              proje görünüm dili
+            </q-item-selection>
+            <q-item-selection>
+              <q-icon name="keyboard_arrow_right" />
+            </q-item-selection>
+            <q-menu auto-close anchor="top end" self="top start">
+              <q-list>
+                <q-item>
+                  <q-item-selection>
+                    türkçe
+                  </q-item-selection>
+                </q-item>
+                <q-item>
+                  <q-item-selection>
+                    ingilizce
+                  </q-item-selection>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-item>
+        </q-list>
+      </q-menu>
+    </q-btn>
+    <q-btn label="boyahane" flat>
+      <q-menu>
+        <q-list>
+          <q-item>
+            <q-item-selection>
+              cihaz kullanıcıları
+            </q-item-selection>
+          </q-item>
+        </q-list>
+      </q-menu>
+    </q-btn>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
