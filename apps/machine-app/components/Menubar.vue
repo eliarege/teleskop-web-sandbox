906b4d7d6f1176fdb27b243ce748aad8f18e682d
EkinBarisCakir <baris.cakir@eliar.com.tr>
2023-10-05 11:44:21 +0300

    feat: add bordered card to menu bar

--- apps/machine-app/components/Menubar.vue
+++ apps/machine-app/components/Menubar.vue
@@ -2,19 +2,20 @@
 </script>
 
 <template>
-  <div class="flex flex-row justify-between">
-    <div class="flex flex-row items-center">
+  <q-card class="flex flex-row justify-between" bordered>
+    <q-card-section class="flex items-center">
       <q-btn label="Yeni" no-caps icon="note_add" color="primary" class="mr-4 ml-2" />
       <q-btn label="Proje Yükle" no-caps icon="system_update_alt" color="primary" class="mr-4" />
       <q-btn label="Özellikler" no-caps icon="tune" color="primary" class="mr-4" />
       <q-btn label="Sil" no-caps icon="delete" color="primary" class="mr-4" />
       <q-btn label="Versiyon Bilgisi Al" no-caps color="primary" class="mr-4" />
-    </div>
-    <div class="flex flex-row items-end mr-8">
+    </q-card-section>
+
+    <q-card-section class="flex flex-row items-end mr-8">
       <q-chip class="mr-4 mb-2">
         DB v3.3.95.0
       </q-chip>
-      <q-card class="flex flex row mb-2 h-25 items-center">
+      <q-card class="flex flex row mb-2 items-center">
         <q-card-section>
           Hoşgeldiniz Barış
         </q-card-section>
@@ -24,8 +25,8 @@
           </q-btn>
         </q-card-section>
       </q-card>
-    </div>
-  </div>
+    </q-card-section>
+  </q-card>
 </template>
 
 <style scoped>
