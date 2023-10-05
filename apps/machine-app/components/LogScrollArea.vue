d5e60110411daa788d3d0e35a1613a60c253b881
EkinBarisCakir <baris.cakir@eliar.com.tr>
2023-10-05 11:54:52 +0300

    feat: add app title

--- apps/machine-app/components/LogScrollArea.vue
+++ apps/machine-app/components/LogScrollArea.vue
@@ -5,7 +5,7 @@
 <template>
   <q-scroll-area class="h-50">
     <div v-for="n in 100" :key="n" class="ml-2">
-      jdlasjdl
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed labore deserunt asperiores, illo laudantium quam fugit officia neque veritatis voluptatem!
     </div>
   </q-scroll-area>
 </template>
--- apps/machine-app/pages/index.vue
+++ apps/machine-app/pages/index.vue
@@ -3,6 +3,9 @@
 </script>
 
 <template>
+  <q-chip outline color="teal">
+    Teleskop Basic - Makineler 4.19
+  </q-chip>
   <Topbar />
   <Menubar />
   <Table />
