9ef0a95d3b02695a9dbcc8b7cba5efe8b243df96
Alper Atmaca <alper.atmaca@eliarge.com>
2023-10-05 12:17:31 +0300

    chore: added imports to index

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,1 +1,4 @@
-export { default as NoVnc, NoVncProps } from './components/NoVnc.vue'
+export { default as NoVnc } from './components/NoVnc.vue'
+export { default as LoadingSpinner } from './components/LoadingSpinner.vue'
+export { default as LoadingScreen } from './components/LoadingScreen.vue'
+export type { NoVncProps } from './components/NoVnc.vue'
