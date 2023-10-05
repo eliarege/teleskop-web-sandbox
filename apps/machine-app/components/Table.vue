e137eb7663b5d409b713e54f7caa6ac61bc2fd34
EkinBarisCakir <baris.cakir@eliar.com.tr>
2023-10-05 11:19:19 +0300

    feat: table remove pagination, add columns, add multiple selection

--- apps/machine-app/components/Table.vue
+++ apps/machine-app/components/Table.vue
@@ -25,6 +25,45 @@ const columns = [
     field: (row: any) => row.model,
     align: 'left',
   },
+
+  {
+    name: 'version',
+    label: 'Versiyon',
+    field: (row: any) => row.version,
+    align: 'left',
+  },
+  {
+    name: 'machineCapacity',
+    label: 'Makine Kapasitesi',
+    field: (row: any) => row.machineCapacity,
+    align: 'left',
+  },
+
+  {
+    name: 'towerCount',
+    label: 'Kule Sayısı',
+    field: (row: any) => row.towerCount,
+    align: 'left',
+  },
+  {
+    name: 'machineIP',
+    label: 'Makine IP',
+    field: (row: any) => row.machineIP,
+    align: 'left',
+  },
+  {
+    name: 'atUse',
+    label: 'Kullanımda',
+    field: (row: any) => row.atUse,
+    align: 'left',
+  },
+  {
+    name: 'PLCModel',
+    label: 'PLC Modeli',
+    field: (row: any) => row.PLCModel,
+    align: 'left',
+  },
+
 ]
 const rows = [
   {
@@ -32,12 +71,24 @@ const rows = [
     machineName: '900HT3',
     group: 'HT-2',
     model: 'T7000',
+    version: '3.19.1.RC.16',
+    machineCapacity: 540,
+    towerCount: 3,
+    machineIP: '10.0.10.4',
+    atUse: true,
+    PLCModel: 'TBBPLC',
   },
   {
     id: 2,
     machineName: '900HT3',
     group: 'HT-2',
     model: 'T7000',
+    version: '3.19.1.RC.16',
+    machineCapacity: 540,
+    towerCount: 3,
+    machineIP: '10.0.10.4',
+    atUse: true,
+    PLCModel: 'TBBPLC',
   },
 
   {
@@ -45,6 +96,12 @@ const rows = [
     machineName: '900HT3',
     group: 'HT-2',
     model: 'T7000',
+    version: '3.19.1.RC.16',
+    machineCapacity: 540,
+    towerCount: 3,
+    machineIP: '10.0.10.4',
+    atUse: true,
+    PLCModel: 'TBBPLC',
   },
 
   {
@@ -52,25 +109,114 @@ const rows = [
     machineName: '900HT3',
     group: 'HT-2',
     model: 'T7000',
+    version: '3.19.1.RC.16',
+    machineCapacity: 540,
+    towerCount: 3,
+    machineIP: '10.0.10.4',
+    atUse: true,
+    PLCModel: 'TBBPLC',
   },
 
   {
-    id: 1,
+    id: 5,
     machineName: '900HT3',
     group: 'HT-2',
     model: 'T7000',
+    version: '3.19.1.RC.16',
+    machineCapacity: 540,
+    towerCount: 3,
+    machineIP: '10.0.10.4',
+    atUse: true,
+    PLCModel: 'TBBPLC',
+  },
+
+  {
+    id: 6,
+    machineName: '900HT3',
+    group: 'HT-2',
+    model: 'T7000',
+    version: '3.19.1.RC.16',
+    machineCapacity: 540,
+    towerCount: 3,
+    machineIP: '10.0.10.4',
+    atUse: true,
+    PLCModel: 'TBBPLC',
+  },
+
+  {
+    id: 7,
+    machineName: '900HT3',
+    group: 'HT-2',
+    model: 'T7000',
+    version: '3.19.1.RC.16',
+    machineCapacity: 540,
+    towerCount: 3,
+    machineIP: '10.0.10.4',
+    atUse: true,
+    PLCModel: 'TBBPLC',
+  },
+
+  {
+    id: 8,
+    machineName: '900HT3',
+    group: 'HT-2',
+    model: 'T7000',
+    version: '3.19.1.RC.16',
+    machineCapacity: 540,
+    towerCount: 3,
+    machineIP: '10.0.10.4',
+    atUse: true,
+    PLCModel: 'TBBPLC',
+  },
+
+  {
+    id: 9,
+    machineName: '900HT3',
+    group: 'HT-2',
+    model: 'T7000',
+    version: '3.19.1.RC.16',
+    machineCapacity: 540,
+    towerCount: 3,
+    machineIP: '10.0.10.4',
+    atUse: true,
+    PLCModel: 'TBBPLC',
+  },
+
+  {
+    id: 10,
+    machineName: '900HT3',
+    group: 'HT-2',
+    model: 'T7000',
+    version: '3.19.1.RC.16',
+    machineCapacity: 540,
+    towerCount: 3,
+    machineIP: '10.0.10.4',
+    atUse: true,
+    PLCModel: 'TBBPLC',
   },
 
 ]
+
+const pagination = { rowsPerPage: 0 }
+const selected = ref([])
 </script>
 
 <template>
   <q-table
+    v-model:selected="selected"
+    :pagination="pagination"
     :rows="rows"
     :columns="columns"
+    hide-pagination
+    separator="cell"
+    bordered
+    dense
+    selection="multiple"
   />
 </template>
 
 <style scoped>
-
+.text-header {
+  font-weight: bold;
+}
 </style>
