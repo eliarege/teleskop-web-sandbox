fbac7a312ddab5b1fbb37a15b4e1a819b145ffb5
EkinBarisCakir <baris.cakir@eliar.com.tr>
2023-10-10 17:47:59 +0300

    feat: add machine add, machine delete, machine group queries

--- apps/machines/server/api/[...machine].ts
+++ apps/machines/server/api/[...machine].ts
@@ -30,8 +30,38 @@ router.get('/machines', defineEventHandler(async () => {
 
 router.get('/machine-group', defineEventHandler(async () => {
   try {
-    const machineGroups = await knex('BFMACHGROUP').select('GROUPNAME')
-    return machineGroups.map(obj => obj.GROUPNAME)
+    const machineGroups = await knex('BFMACHGROUP').select('GROUPNAME', 'GROUPID')
+    return machineGroups
+  } catch (e) {
+    return e
+  }
+}))
+
+router.post('/machine-add', defineEventHandler(async (event) => {
+  try {
+    const machine = await readBody(event)
+    const res = await knex('BFMACHINES').insert({
+      MACHINEID: machine.no,
+      MACHINECODE: machine.name,
+      GRUPNO: machine.group.GROUPID,
+      TBBMODEL: machine.model,
+      THEORICALCHARGE: machine.theoricalCharge,
+      MACHINECAPACITY: machine.machineCapacity,
+      IP: machine.ip,
+      PORT: -1,
+    })
+    return res
+  } catch (e) {
+    console.error(e)
+    return e
+  }
+}))
+
+router.post('/machine-delete', defineEventHandler(async (event) => {
+  try {
+    const { machineIds } = await readBody(event)
+    const res = await knex('BFMACHINES').whereIn('MACHINEID', machineIds).del()
+    return res
   } catch (e) {
     return e
   }
