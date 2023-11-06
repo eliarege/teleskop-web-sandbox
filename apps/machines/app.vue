9022ef16b9e830223032d5419a7ba38081e92260
Alper Atmaca <alper.atmaca@eliarge.com>
2023-11-06 17:17:57 +0300

    chore(machines): removed layout folder

--- apps/machines/app.vue
+++ apps/machines/app.vue
@@ -1,7 +1,198 @@
+<script setup lang="ts">
+const definitionItems = [
+  {
+    label: 'Makine',
+    items: [
+      {
+        label: 'Yeni',
+      },
+      {
+        label: 'Özellikler',
+      },
+
+      {
+        label: 'Sil',
+      },
+
+      {
+        label: 'Yükle',
+      },
+
+      {
+        label: 'Çıkış',
+      },
+    ],
+  },
+  {
+    label: 'Makine Grubu',
+    items: [
+      {
+        label: 'Değiştir',
+      },
+    ],
+  },
+  {
+    label: 'Ayarlar',
+    items: [
+      {
+        label: 'Cihaz Ayarları',
+        items: [
+          {
+            label: 'Cihaz Kullanıcıları',
+            to: '/user-definitions',
+          },
+          {
+            label: 'Cihaz Erişim Hataları',
+          },
+
+          {
+            label: 'Cihaz Kapalı Zamanlar',
+          },
+
+          {
+            label: 'Komut Tipi Tanımları',
+          },
+
+          {
+            label: 'Manuele Alma Nedenleri',
+            to: '/manual-reasons',
+          },
+
+          {
+            label: 'Makine Duruş Nedenleri',
+            to: '/machine-stop-reasons',
+          },
+
+          {
+            label: 'Makine Bitirme Nedenleri',
+            to: '/machine-finish-reasons',
+          },
+
+          {
+            label: 'Adım Atlatma Ayarları',
+          },
+
+          {
+            label: 'Akıllı İstek Komut Tanımları',
+          },
+
+          {
+            label: 'Diğer Makineler',
+          },
+        ],
+      },
+      {
+        label: 'İlave Kabı Ayarları',
+        items: [
+          {
+            label: 'Kazan Tanımları',
+          },
+          {
+            label: 'Kazan-Materyal Eşleştirme',
+          },
+          {
+            label: 'Kazan-Materyal Su Limit Tanımları',
+          },
+        ],
+      },
+      {
+        label: 'Entegrasyon Ayarları',
+        items: [
+          {
+            label: 'ERP Parametreleri',
+          },
+          {
+            label: 'Başlatma Parametresi Tipleri',
+          },
+          {
+            label: 'Reçete Tipleri',
+          },
+          {
+            label: 'Optimizasyon Parametreleri',
+          },
+        ],
+      },
+      {
+        label: 'Tüketim Ayarları',
+        items: [
+          {
+            label: 'Su Tipleri',
+          },
+          {
+            label: 'Tüketim Sayaç Seçimi',
+          },
+          {
+            label: 'Teorik Su Tüketimi Ayarları',
+          },
+        ],
+      },
+      {
+        label: 'Uygulama Ayarları',
+        items: [
+          {
+            label: 'Teleskop Ayarları',
+          },
+          {
+            label: 'Proje Görünüm Dili',
+          },
+        ],
+      },
+      {
+        label: 'Bildirim',
+        items: [
+          {
+            label: 'E-posta Bildirim Ayarları',
+          },
+        ],
+      },
+    ],
+
+  },
+
+]
+</script>
+
 <template>
   <div>
     <NuxtLayout>
-      <NuxtPage />
+      <div class="relative">
+        <q-chip outline color="teal">
+          Teleskop Basic - Makineler 4.19
+        </q-chip>
+        <div class="flex flex-row">
+          <DropdownMenu
+            v-for="(item, idx) in definitionItems"
+            :key="idx"
+            :model="item"
+            strategy="fixed"
+            placement="bottom"
+            second-placement="right-start"
+            class="z-10"
+          >
+            <template #default="{ item, depth, active }">
+              <div
+                v-if="depth === 0"
+                class="cursor-pointer whitespace-nowrap select-none text-black text-center gap-1 hover:(bg-light-400 text-gray-800) rounded p-2 lg:px-3 xl:px-4  transition-all"
+                @click="item.onClick"
+              >
+                {{ item.label }}
+              </div>
+              <div v-else class="w-full cursor-pointer bg-white text-black border px-3 py-2 flex items-center gap-3 justify-between whitespace-nowrap hover:(important-bg-light-400 important-text-gray-800)">
+                {{ item.label }}
+                <i v-if="item.items" class="fa fa-chevron-right" />
+                <q-btn
+                  v-if="item.items"
+                  class="text-xs"
+                  flat
+                  no-caps
+                  icon-right="chevron_right"
+                />
+              </div>
+            </template>
+          </DropdownMenu>
+        </div>
+        <NuxtPage />
+      </div>
     </NuxtLayout>
   </div>
 </template>
