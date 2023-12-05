9cef77ef82ff06b372b4f140d20c67e54ece836a
Hasan Can Erol <hasancan.erol@eliarge.com>
2023-12-05 12:22:23 +0300

    feat: settings deletion confirmation dialog

---
+++ apps/dispensing-manager-ui/components/SettingsConfirmationDiaog.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const props = defineProps({
+  visible: Boolean,
+})
+const { t } = useI18n()
+const confirm = ref(!!props.visible)
+</script>
+
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-dialog v-model="confirm" persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <q-avatar
+            icon="delete"
+            color="white"
+            text-color="delete"
+          />
+          <span class="q-ml-sm"> {{ t('warnings.deleteRow') }}</span>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn
+            v-close-popup
+            :label="t('settings.cancel')"
+            outline
+            color="black"
+            icon="close"
+          />
+          <q-btn
+            v-close-popup
+            outline
+            :label="t('settings.delete')"
+            color="red"
+            icon="delete"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<style>
+</style>
