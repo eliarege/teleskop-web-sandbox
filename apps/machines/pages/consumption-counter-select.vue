205b0f26d46facd11d4944457bb7984c40fc88d2
EkinBarisCakir <baris.cakir@eliar.com.tr>
2023-12-05 14:43:54 +0300

    feat: add consumption counter select

--- apps/machines/pages/consumption-counter-select.vue
+++ apps/machines/pages/consumption-counter-select.vue
@@ -1,5 +1,36 @@
 <script setup lang="ts">
-const { data: machines } = await useFetch('/api/command-timeout-reasons/command-map-machines')
+const selectedMachineId = ref()
+
+const counter1 = ref()
+const counter2 = ref()
+
+const { data: machines } = useLazyFetch('/api/machines/active-machines')
+
+const { data: counterOptions } = useLazyFetch('/api/consumption-counters/mach-counters', {
+  immediate: false,
+  query: { machineId: selectedMachineId },
+  transform: (counterOptions) => {
+    counterOptions.unshift({
+      id: -1,
+      name: 'Seçilmedi',
+    })
+    return counterOptions
+  },
+})
+
+const { data: counters } = useLazyFetch('/api/consumption-counters/consumption-counters', {
+  immediate: false,
+  query: { machineId: selectedMachineId },
+})
+
+watch(counters, (newValue, oldValue) => {
+  counter1.value = counterOptions.value.find(option => option.id === counters.value.counter1)?.name || ''
+  counter2.value = counterOptions.value.find(option => option.id === counters.value.counter2)?.name || ''
+})
+
+async function handleMachineClick(machineId: number) {
+  selectedMachineId.value = machineId
+}
 </script>
 
 <template>
@@ -12,6 +43,7 @@ const { data: machines } = await useFetch('/api/command-timeout-reasons/command-
           :key="machine.machineId"
           v-ripple
           clickable
+          @click="handleMachineClick(machine.machineId)"
         >
           <q-item-section>
             {{ machine.machineName }}
@@ -20,13 +52,28 @@ const { data: machines } = await useFetch('/api/command-timeout-reasons/command-
       </q-list>
     </q-card-section>
 
-    <q-card-section class="flex flex-col">
-      <q-input label="Sayaç 1" />
-      <q-input label="Sayaç 2" />
+    <q-card-section class="flex flex-col input-field">
+      <q-select
+        v-model="counter1"
+        :options="counterOptions"
+        option-label="name"
+        option-value="id"
+        label="Sayaç 1"
+      />
+      <q-select
+        v-model="counter2"
+        :options="counterOptions"
+        option-label="name"
+        option-value="id"
+        label="Sayaç 2"
+      />
     </q-card-section>
   </q-card>
 </template>
 
 <style scoped>
-
+.input-field>* {
+  min-width: 20em;
+  margin-bottom: 1em;
+}
 </style>
