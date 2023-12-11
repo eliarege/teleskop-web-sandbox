decc6a4929e2dc26b75dab1a38f4aa0e6ed060e6
Ege İliklier <ege.iliklier@eliarge.com>
2023-12-11 21:14:20 +0300

    feat: keycloak for development

---
+++ keycloak/bin/keycloak.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+
+case "$1" in
+  up)
+    shift
+    exec docker compose -f $DIR/../docker-compose.yml -p teleskop up -d $@
+    ;;
+  sync)
+    out=$(script -q /dev/null -c "npx tsx $DIR/sync.ts" | tee /dev/tty)
+    # NodeJS BUG: `fetch` exits the process silently if Keycloak has not been initialized.
+    # If the sync process exits with 0 and has no output, weinfer it's due to the fetch issue.
+    if [[ -z $out && $? -eq 0 ]]; then
+      echo "[ERROR] Keycloak has not been initialized."
+      exit 1
+    fi
+    ;;
+  create | down | kill | restart | pull | start | stop)
+    exec docker compose -f $DIR/../docker-compose.yml -p teleskop $@
+    ;;
+  *)
+    echo "Usage: $0 {up|sync|create|down|kill|restart|pull|start|stop}"
+    ;;
+esac
