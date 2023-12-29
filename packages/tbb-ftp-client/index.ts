d488051055689e76f01c217465b97b9e8c7ef027
Ege İliklier <ege.iliklier@eliarge.com>
2023-12-29 15:12:41 +0300

    feat(tbb-ftp-client): withTbbFtpClient

--- packages/tbb-ftp-client/index.ts
+++ packages/tbb-ftp-client/index.ts
@@ -1,3 +1,18 @@
+import type { TbbFtpClientOptions } from './src/TbbFtpClient'
 import { TbbFtpClient } from './src/TbbFtpClient'
 
 export { TbbFtpClient }
+
+export async function withTbbFtpClient(
+  host: string,
+  callback: (client: TbbFtpClient) => Promise<void> | void,
+  options?: TbbFtpClientOptions,
+) {
+  const client = new TbbFtpClient(host, options)
+  try {
+    await client.connect()
+    await callback(client)
+  } finally {
+    client.close()
+  }
+}
