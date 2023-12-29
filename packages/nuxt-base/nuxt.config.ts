85c8f17ee76d003ac18e8a84276b205d12f2af65
Ege İliklier <ege.iliklier@eliarge.com>
2023-12-29 16:43:23 +0300

    fix: change `kcEnabled` default to `false`

--- packages/nuxt-base/nuxt.config.ts
+++ packages/nuxt-base/nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       kcUrl: 'http://localhost:8080',
       kcRealm: 'teleskop-web',
       kcClientId: 'nuxt-client',
-      kcEnabled: true,
+      kcEnabled: false,
     },
   },
   css: ['@unocss/reset/tailwind.css'],
