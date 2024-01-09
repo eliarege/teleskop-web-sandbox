8105cd7fc54515a3fa5a5e962b0aaed0b21e4ac0
Ege İliklier <ege.iliklier@eliarge.com>
2024-01-09 12:58:16 +0300

    fix(root): use absolute paths to be able run app any directory

--- apps/root/index.js
+++ apps/root/index.js
@@ -1,8 +1,12 @@
 import process from 'node:process'
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
 import express from 'express'
 
 const app = express()
 const port = 4000
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const urls = {
   machines: process.env.MACHINES_URL,
   multiMonitor: process.env.MULTIMONITOR_URL,
@@ -13,17 +17,18 @@ const urls = {
   planningBoard: process.env.PLANNING_BOARD_URL,
 }
 
-app.use(express.static('assets'))
+app.use(express.static(join(__dirname, 'assets')))
 
+app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('index', urls)
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Express server is running on port ${port}`)
 })
 
-process.on('SIGTERM', () => process.exit(0))
-process.on('SIGINT', () => process.exit(0))
+process.on('SIGTERM', () => server.close())
+process.on('SIGINT', () => server.close())
