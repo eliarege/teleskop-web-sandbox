d5547457d4862576ab06f0d9dfa8cedbe2828276
Hasan Can Erol <hasancan.erol@eliarge.com>
2024-02-13 11:42:38 +0300

    chore: columns to computed continues

--- apps/dispensing-manager-ui/components/RecipeStepPreviousRequestsContent.vue
+++ apps/dispensing-manager-ui/components/RecipeStepPreviousRequestsContent.vue
@@ -13,30 +13,14 @@ const data = await $fetch('/api/recipe/previous-requests', {
   method: 'POST',
   body: props,
 })
-const rerequestedStepCols = [
+const rerequestedStepCols = computed(() => [
   { name: 'joborder', label: t('joborder'), field: 'joborder' },
   { name: 'correctionNo', label: t('correctionNo'), field: 'correctionNo' },
   { name: 'mainStep', label: t('recipe.mainStep'), field: 'mainStep' },
   { name: 'status', label: t('statusCodes.text'), field: 'status', format: (val, row) => t(`statusCodes.${val}`), style: row => cellRGBColorHandler(row.status) },
   { name: 'requestTime', label: t('requestTime'), field: 'requestTime', format: (val, row) => val ? d(val, 'datettime') : '_'.repeat(4) },
   { name: 'endTime', label: t('endtime'), field: 'endTime', format: (val, row) => val ? d(val, 'datettime') : '_'.repeat(4) },
-]
-const autoWeiStepCols = [
-  { name: 'materialName', label: t('materialName'), field: 'materialName' },
-  { name: 'parallelStep', label: t('recipe.parallelStep'), field: 'parallelStep' },
-  { name: 'dispenser', label: t('settings.dispName'), field: 'dispenser' },
-  { name: 'status', label: t('statusCodes.text'), field: 'status', format: (val, row) => t(`statusCodes.${val}`), style: row => cellRGBColorHandler(row.status) },
-  { name: 'recipeAmount', label: t('recipeAmount'), field: 'recipeAmount' },
-  { name: 'actualAmount', label: t('actualAmount'), field: 'actualAmount' },
-]
-const manuelWeiStepCols = [
-  { name: 'materialName', label: t('materialName'), field: 'materialName' },
-  { name: 'mainStep', label: t('recipe.mainStep'), field: 'mainStep' },
-  { name: 'parallelStep', label: t('recipe.parallelStep'), field: 'parallelStep' },
-  { name: 'weighingNumber', label: t('weighingInformation.weighingNumber'), field: 'weighingNumber' },
-  { name: 'recipeAmount', label: t('recipeAmount'), field: 'recipeAmount' },
-  { name: 'status', label: t('statusCodes.text'), field: 'status', format: (val, row) => t(`statusCodes.${val}`), style: row => cellRGBColorHandler(row.status) },
-]
+])
 </script>
 
 <template>
@@ -68,19 +52,6 @@ const manuelWeiStepCols = [
           </template>
         </FilterableTable>
       </q-card-section>
-      <!-- <q-card-section>
-        <q-table
-          :title="t('recipe.autoWeiDetails')"
-          :columns="autoWeiStepCols"
-        />
-      </q-card-section>
-      <q-card-section>
-        <q-table
-          :title="t('recipe.manuelWeiDetails')"
-          :columns="manuelWeiStepCols"
-        />
-      </q-card-section> -->
-
       <q-card-actions align="right">
         <q-btn
           v-close-popup
