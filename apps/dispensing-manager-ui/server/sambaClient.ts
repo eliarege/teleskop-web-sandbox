8c11605c504568c455c9be3960c201fba00aa1e2
Hasan Can Erol <hasancan.erol@eliarge.com>
2024-03-01 11:44:29 +0300

    feat: username and password can be configurable on samba client

--- apps/dispensing-manager-ui/server/sambaClient.ts
+++ apps/dispensing-manager-ui/server/sambaClient.ts
@@ -4,6 +4,8 @@ const config = useRuntimeConfig()
 
 const sambaClient = new SambaClient({
   address: config.sambaPath, // required
+  username: config.sambaUser,
+  password: config.sambaPassword,
 })
 
 export { sambaClient }
