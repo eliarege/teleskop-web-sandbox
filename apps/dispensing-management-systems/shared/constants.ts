339bcfc729edd6122e8a62bc6bf6d44ed739b4e2
Onur Enginer <onur.enginer@eliarge.com>
2024-03-26 14:03:34 +0300

    chore: StatusCodes enum added

---
+++ apps/dispensing-management-systems/shared/constants.ts
@@ -0,0 +1,9 @@
+export enum StatusCodes {
+  newRequest = 0,
+  inDispenser = 1,
+  inProcess = 2,
+  requestCompleted = 3,
+  priorityChanged = 4,
+  canceled = 8,
+  dispenserChanged = 10,
+}
--- apps/dispensing-management-systems/store/Colors.ts
+++ apps/dispensing-management-systems/store/Colors.ts
@@ -1,15 +1,17 @@
 import { defineStore } from 'pinia'
+import { StatusCodes } from '~/shared/constants'
 
 export const useColorStore = defineStore('colors', () => {
-  const jobOrderStatusColors: Record<number, string> = {
-    0: '#007BFF',
-    1: '#64ABFC',
-    2: '#73CECE',
-    3: '#4CAF50',
-    4: '#7F72FA',
-    8: '#FF4B4B',
-    10: '#FFBB00',
+  const jobOrderStatusColors: Record<StatusCodes, string> = {
+    [StatusCodes.newRequest]: '#007BFF',
+    [StatusCodes.inDispenser]: '#64ABFC',
+    [StatusCodes.inProcess]: '#73CECE',
+    [StatusCodes.requestCompleted]: '#4CAF50',
+    [StatusCodes.priorityChanged]: '#7F72FA',
+    [StatusCodes.canceled]: '#FF4B4B',
+    [StatusCodes.dispenserChanged]: '#FFBB00',
   }
+
   const colors = {
     jobOrderStatusColors,
     darkJobOrderCellEven: '#ACACAC',
