61993df140ce172bda7e4f35023ef9c28c010023
EkinBarisCakir <baris.cakir@eliar.com.tr>
2024-04-01 17:48:36 +0300

    fix: remove stepper control in only formkit inputs

--- apps/machines/components/FormTableKit.vue
+++ apps/machines/components/FormTableKit.vue
@@ -148,16 +148,3 @@ watch(showModal, async (newValue, _oldValue) => {
     </q-card>
   </q-dialog>
 </template>
-
-<style>
-input[type="number"]::-webkit-inner-spin-button,
-input[type="number"]::-webkit-outer-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-input[type="number"] {
-  -moz-appearance: textfield; /* Firefox */
-  appearance: textfield; /* Standard */
-}
-</style>
