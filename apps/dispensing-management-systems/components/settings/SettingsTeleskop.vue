e2c744d3f3918d87cb71328b3a086a98bf5a1bb9
Onur Enginer <onur.enginer@eliarge.com>
2024-04-03 10:32:34 +0300

    fix: SettingsTeleskop layout fixes

--- apps/dispensing-management-systems/components/settings/SettingsTeleskop.vue
+++ apps/dispensing-management-systems/components/settings/SettingsTeleskop.vue
@@ -28,102 +28,100 @@ function onReset() {
 </script>
 
 <template>
-  <div>
-    <div class="content-section">
-      <QForm @submit.prevent>
-        <div class="align-start justify-center">
-          <div class="text-xl flex flex-center">
-            {{ t('settings.Teleskop') }}
-          </div>
-          <div class="flex-center flex-col text-size-4 w-full ">
-            <div class="flex flex-row flex-center">
-              <div class="row-item">
-                <span class="w-24">
-                  {{ t('protocolParameters.dbClient') }}
-                </span>
-                <QInput
-                  v-model="teleskopSettings!.client"
-                  dense
-                  type="text"
-                  filled
-                />
-              </div>
-              <div class="row-item">
-                {{ t('protocolParameters.dbHostComputer') }}
-                <QInput
-                  v-model="teleskopSettings!.hostComputer"
-                  dense
-                  type="text"
-                  filled
-                />
-              </div>
-            </div>
-            <div class="flex flex-row flex-center">
-              <div class="row-item">
-                {{ t('protocolParameters.dbName') }}
-                <QInput
-                  v-model="teleskopSettings!.database"
-                  dense
-                  type="text"
-                  filled
-                />
-              </div>
-              <div class="row-item">
-                {{ t('protocolParameters.dbHost') }}
-                <QInput
-                  v-model="teleskopSettings!.host"
-                  dense
-                  type="text"
-                  filled
-                />
-              </div>
-            </div>
-            <div class="flex flex-row flex-center">
-              <div class="row-item">
-                {{ t('protocolParameters.dbPort') }}
-                <QInput
-                  v-model="teleskopSettings!.port"
-                  class="select-item"
-                  dense
-                  filled
-                  type="number"
-                  min="0"
-                  hide-bottom-space
-                  :rules="[(val: number) => val >= 0]"
-                />
-              </div>
-
-              <div class="row-item">
-                {{ t('protocolParameters.dbUser') }}
-                <QInput
-                  v-model="teleskopSettings!.user"
-                  dense
-                  type="text"
-                  filled
-                />
-              </div>
+  <div class="content-section">
+    <QForm @submit.prevent>
+      <div class="align-start justify-center">
+        <div class="text-xl flex flex-center">
+          {{ t('settings.Teleskop') }}
+        </div>
+        <div class="flex-center flex-col text-size-4 w-full ">
+          <div class="flex flex-row flex-center">
+            <div class="row-item">
+              <span class="w-24">
+                {{ t('protocolParameters.dbClient') }}
+              </span>
+              <QInput
+                v-model="teleskopSettings!.client"
+                dense
+                type="text"
+                filled
+              />
             </div>
             <div class="row-item">
-              {{ t('protocolParameters.dbPassword') }}
+              {{ t('protocolParameters.dbHostComputer') }}
               <QInput
-                v-model="teleskopSettings!.password"
+                v-model="teleskopSettings!.hostComputer"
                 dense
-                :type="passwordVisible ? 'text' : 'password'"
+                type="text"
                 filled
-              >
-                <template #append>
-                  <QIcon
-                    :name="passwordVisible ? 'visibility_off' : 'visibility'"
-                    class="cursor-pointer"
-                    @click="passwordVisible = !passwordVisible"
-                  />
-                </template>
-              </QInput>
+              />
             </div>
           </div>
+          <div class="flex flex-row flex-center">
+            <div class="row-item">
+              {{ t('protocolParameters.dbName') }}
+              <QInput
+                v-model="teleskopSettings!.database"
+                dense
+                type="text"
+                filled
+              />
+            </div>
+            <div class="row-item">
+              {{ t('protocolParameters.dbHost') }}
+              <QInput
+                v-model="teleskopSettings!.host"
+                dense
+                type="text"
+                filled
+              />
+            </div>
+          </div>
+          <div class="flex flex-row flex-center">
+            <div class="row-item">
+              {{ t('protocolParameters.dbPort') }}
+              <QInput
+                v-model="teleskopSettings!.port"
+                class="select-item"
+                dense
+                filled
+                type="number"
+                min="0"
+                hide-bottom-space
+                :rules="[(val: number) => val >= 0]"
+              />
+            </div>
+
+            <div class="row-item">
+              {{ t('protocolParameters.dbUser') }}
+              <QInput
+                v-model="teleskopSettings!.user"
+                dense
+                type="text"
+                filled
+              />
+            </div>
+          </div>
+          <div class="row-item">
+            {{ t('protocolParameters.dbPassword') }}
+            <QInput
+              v-model="teleskopSettings!.password"
+              dense
+              :type="passwordVisible ? 'text' : 'password'"
+              filled
+            >
+              <template #append>
+                <QIcon
+                  :name="passwordVisible ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="passwordVisible = !passwordVisible"
+                />
+              </template>
+            </QInput>
+          </div>
         </div>
-      </QForm>
-    </div>
+      </div>
+    </QForm>
     <div class="buttons-section">
       <div class="flex-center justify-evenly">
         <QBtn
@@ -146,7 +144,7 @@ function onReset() {
 
 <style scoped>
 .content-section {
-  height: 100vh;
+  flex: 1;
   overflow-y: auto;
 }
 .buttons-section {
@@ -154,9 +152,7 @@ function onReset() {
   justify-content: center;
   align-items: center;
   height: 10vh;
-  position: sticky;
   bottom: 0;
-  width: 88%;
   background-color: white;
 }
 .body--dark .buttons-section {
