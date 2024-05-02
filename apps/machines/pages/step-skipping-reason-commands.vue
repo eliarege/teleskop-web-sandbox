f569a1d1bffa535b2374980165982cf01b84a83f
EkinBarisCakir <baris.cakir@eliar.com.tr>
2024-05-03 01:48:36 +0300

    feat: add submit for step skipping reason commands

--- apps/machines/pages/step-skipping-reason-commands.vue
+++ apps/machines/pages/step-skipping-reason-commands.vue
@@ -2,6 +2,7 @@
 const { t } = useI18n()
 
 const selectedMachineId = ref()
+const changedCommands = ref([])
 
 const { data: machines } = useLazyFetch('/api/machines/active-machines')
 
@@ -24,57 +25,78 @@ const { data: commands } = useLazyFetch('/api/master-commands/master-commands', 
 
 async function handleCheckChange(e, command) {
   command.machineId = selectedMachineId.value
-  if (e)
-    await checkStepSkippingReason(command)
-  else if (!e)
-    await uncheckStepSkippingReason(command)
+
+  changedCommands.value.push({ command, checked: e })
+}
+
+async function handleSubmit() {
+  await $fetch('/api/step-skipping-reasons/command-reason', {
+    method: 'PUT',
+    body: { changedCommands: changedCommands.value },
+  })
 }
 </script>
 
 <template>
-  <q-card class="flex flex-row justify-center">
-    <q-card-section class="w-sm">
-      <h3>{{ t('machines') }}</h3>
-      <q-list bordered separator>
-        <q-item
-          v-for="machine in machines"
-          :key="machine.machineId"
-          v-ripple
-          clickable
-          :active="selectedMachineId === machine.machineId"
-          :focused="selectedMachineId === machine.machineId"
-          @click="selectedMachineId = machine.machineId"
+  <q-card>
+    <q-card-section class="flex flex-row justify-center gap-8">
+      <div class="w-sm">
+        <h3>{{ t('machines') }}</h3>
+        <q-list bordered separator>
+          <q-item
+            v-for="machine in machines"
+            :key="machine.machineId"
+            v-ripple
+            clickable
+            :active="selectedMachineId === machine.machineId"
+            :focused="selectedMachineId === machine.machineId"
+            @click="selectedMachineId = machine.machineId"
+          >
+            <q-item-section>
+              {{ machine.machineCode }}
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+
+      <div class="w-sm">
+        <h3>{{ t('commandList') }}</h3>
+        <q-list
+          bordered
+          separator
+          class="overflow-y-auto h-160"
         >
-          <q-item-section>
-            {{ machine.machineCode }}
-          </q-item-section>
-        </q-item>
-      </q-list>
+          <q-item
+            v-for="command in commands"
+            :key="command.commandNo"
+            v-ripple
+            clickable
+          >
+            <q-item-section>
+              <q-checkbox
+                v-model:model-value="command.checked"
+                :label="command.commandName"
+                @update:model-value="(e) => handleCheckChange(e, command)"
+              />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
     </q-card-section>
 
-    <q-card-section class="w-sm">
-      <h3>{{ t('commandList') }}</h3>
-      <q-list
-        bordered
-        separator
-        class="overflow-y-auto h-160"
-      >
-        <q-item
-          v-for="command in commands"
-          :key="command.commandNo"
-          v-ripple
-          clickable
-        >
-          <q-item-section>
-            <q-checkbox
-              v-model:model-value="command.checked"
-              :label="command.commandName"
-              @update:model-value="(e) => handleCheckChange(e, command)"
-            />
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-card-section>
+    <q-card-actions align="right" class="mt-4 mr-4">
+      <q-btn
+        no-caps
+        :label="t('cancel')"
+        @click="$router.go(0)"
+      />
+      <q-btn
+        color="primary"
+        no-caps
+        :label="t('submit')"
+        @click="handleSubmit"
+      />
+    </q-card-actions>
   </q-card>
 </template>
 
