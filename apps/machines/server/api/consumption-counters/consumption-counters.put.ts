a28c3956b147245b2760bff2dbac65ee413d1c62
EkinBarisCakir <baris.cakir@eliar.com.tr>
2024-05-06 02:01:06 +0300

    feat: add submit for consumption counter select

---
+++ apps/machines/server/api/consumption-counters/consumption-counters.put.ts
@@ -0,0 +1,28 @@
+import { knex } from '~/server/connectionPool'
+
+export default defineEventHandler(async (event) => {
+  const { changedCounters } = await readBody(event)
+
+  for (const counter of changedCounters) {
+    const { machineId, counterId1, counterId2 } = counter
+
+    const existingRecord = await knex('BFCONSUMPTIONCOUNTERS')
+      .where('MACHINEID', machineId)
+      .first()
+
+    if (existingRecord) {
+      await knex('BFCONSUMPTIONCOUNTERS')
+        .where('MACHINEID', machineId)
+        .update({
+          COUNTER1: counterId1,
+          COUNTER2: counterId2,
+        })
+    } else {
+      await knex('BFCONSUMPTIONCOUNTERS').insert({
+        MACHINEID: machineId,
+        COUNTER1: counterId1,
+        COUNTER2: counterId2,
+      })
+    }
+  }
+})
