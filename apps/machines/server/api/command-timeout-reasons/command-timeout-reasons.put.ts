3696d06ad7e0425a6eed865de965e7011b9bd769
EkinBarisCakir <baris.cakir@eliar.com.tr>
2024-05-06 02:23:54 +0300

    feat: add submit for command timeout reasons

---
+++ apps/machines/server/api/command-timeout-reasons/command-timeout-reasons.put.ts
@@ -0,0 +1,22 @@
+import { knex } from '~/server/connectionPool'
+
+export default defineEventHandler(async (event) => {
+  const {changedReasons} = await readBody(event)
+  for (const reason of changedReasons) {
+  const { id, machineId, commandNo, checked } = reason
+
+checked ?
+  await knex('BFCOMMANDTIMEOUTREASONMAP').insert({
+    REASONID: id,
+    MACHINEID: machineId,
+    COMMANDNO: commandNo,
+  })
+:
+  await knex('BFCOMMANDTIMEOUTREASONMAP').where({
+    REASONID: id,
+    MACHINEID: machineId,
+    COMMANDNO: commandNo,
+  }).del()
+
+  }
+})
