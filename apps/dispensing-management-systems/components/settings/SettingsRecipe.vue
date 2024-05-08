8b8ffab79566aa047d2a7e923fb23fc4682e335b
Onur Enginer <onur.enginer@eliarge.com>
2024-05-08 15:18:33 +0300

    chore: SettingsRecipe table

--- apps/dispensing-management-systems/components/settings/SettingsRecipe.vue
+++ apps/dispensing-management-systems/components/settings/SettingsRecipe.vue
@@ -1,9 +1,66 @@
 <script setup lang="ts">
+import type { QTableColumn } from 'quasar'
+import type { RecipeMaster } from '~/shared/types';
 
+const { t } = useI18n()
+const { data: recipes, refresh: refreshRecipes } = await useFetch<RecipeMaster[]>('/api/recipes/master')
+  const columns: (QTableColumn<RecipeMaster>)[] = [
+  {
+    name: 'recipeId',
+    label: t('recipeFields.ID'),
+    field: 'recipeId',
+    sortable: true,
+    align: 'left',
+  },
+  {
+    name: 'recipeName',
+    label: t('recipeFields.Name'),
+    field: 'recipeName',
+    sortable: true,
+    align: 'left',
+  }
+]
+
+async function onRowClick(_event: Event, row: any) {
+
+}
+async function addNewRecipe() {
+
+}
+const pagination = ref({ rowsPerPage: 20 })
 </script>
 
 <template>
-  <div />
+  <div class="flex-center text-xl">
+    {{ t('settings.Recipe') }}
+  </div>
+  <QSeparator
+    class="w-full mt-5 mb-5"
+  />
+  <div class="flex-center mb-4">
+    <QBtn
+      :label="$t('AddNewRecipe')"
+      no-caps
+      icon="note_add"
+      color="primary"
+      class="h-12"
+      style="white-space: nowrap; text-overflow: ellipsis;"
+      clickable
+      @click="addNewRecipe"
+    />
+  </div>
+  <QTable
+    flat
+    bordered
+    table-header-class="table-header"
+    table-class="max-h-150"
+    separator="cell"
+    :pagination
+    :columns="columns"
+    :rows="recipes"
+    row-key="name"
+    @row-click="onRowClick"
+  />
 </template>
 
 <style scoped lang="postcss">
