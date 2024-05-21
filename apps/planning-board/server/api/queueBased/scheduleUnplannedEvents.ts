a50c89a32a914541d0d65bdac61cf4b53c1972ae
Alper Atmaca <alper.atmaca@eliarge.com>
2024-05-21 12:12:58 +0300

    fix: removed console.log

--- apps/planning-board/server/api/queueBased/scheduleUnplannedEvents.ts
+++ apps/planning-board/server/api/queueBased/scheduleUnplannedEvents.ts
@@ -3,7 +3,6 @@ import type { QueueBasedPlannedEvents } from '~/shared/queueBased'
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
   const { newEvent } = await readBody(event)
-  console.log(newEvent)
   const url = `${config.planningEngineUrl}/queue_based/schedule_unplanned_events`
   const events = $fetch<QueueBasedPlannedEvents[]>(url, {
     method: 'POST',
