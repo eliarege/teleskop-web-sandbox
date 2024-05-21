249b720284af6deb72974372fab8ae01c2b7bb30
Onur Enginer <onur.enginer@eliarge.com>
2024-05-21 15:11:54 +0300

    fix(dms): MainMenu sync removed and margin added

--- apps/dispensing-management-systems/components/MainMenu.vue
+++ apps/dispensing-management-systems/components/MainMenu.vue
@@ -1,33 +1,17 @@
 <script lang="ts" setup>
-import { useStateStore } from '~/store/State'
 
 const { t } = useI18n()
-const stateStore = useStateStore()
 
 function searchJobOrder() {
   navigateTo({
     path: '/jobOrders',
   })
 }
-async function syncData() {
-  try {
-    stateStore.isLoading = true
-    await $fetch('/api/teleskop/sync')
-  } catch (e) {
-    console.error(e)
-  } finally {
-    stateStore.isLoading = false
-    onRefresh()
-  }
-}
-function onRefresh() {
-  window.location.reload()
-}
 </script>
 
 <template>
   <div>
-    <h3 class="text-center text-8 opacity-50">
+    <h3 class="text-center text-8 opacity-50 px-20">
       {{ t('SelectDispenser') }}
     </h3>
     <div class="flex-center mt-2">
@@ -39,25 +23,18 @@ function onRefresh() {
     </div>
     <div class="flex justify-evenly items-center w-full mt-15 ">
       <QBtn
-        class="h-50 w-60 pb-15 pt-15 mt-2 mr-2 opacity-75 border border-solid text-5 b-rd-2"
+        class="h-50 w-60 pb-15 pt-15 mt-2 opacity-75 border border-solid text-5 b-rd-2"
         no-caps
         icon="add"
         :label="t('AddNew')"
         @click="navigateTo(`/machines`)"
       />
       <QBtn
-        class="h-50 w-60 pb-15 pt-15 pl-15 pr-15 mt-2 ml-2 opacity-75 border border-solid text-5 b-rd-2"
+        class="h-50 w-60 pb-15 pt-15 pl-15 pr-15 mt-2 opacity-75 border border-solid text-5 b-rd-2"
         no-caps
         icon="search"
         :label="t('SearchJob')"
         @click="searchJobOrder"
-      />
-      <QBtn
-        class="h-40 w-50 pb-10 pt-10 opacity-75 border border-solid text-5 b-rd-2 hidden"
-        no-caps
-        icon="sync"
-        :label="t('SyncData')"
-        @click="syncData"
       />
     </div>
   </div>
