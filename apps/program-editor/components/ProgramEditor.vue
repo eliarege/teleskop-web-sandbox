63fd3d543631f4f7061aa2814d69c1e4292ee2db
Mücahit Yaman <mucahit.yaman@eliarge.com>
2024-05-24 00:21:00 +0300

    feat: Sortable was added in ProgramEditor, drag drop was added, buttons were updated, css corrections were made.

--- apps/program-editor/components/ProgramEditor.vue
+++ apps/program-editor/components/ProgramEditor.vue
@@ -1,69 +1,98 @@
 <script setup lang="ts">
+import { Sortable } from 'sortablejs-vue3'
+import type { SortableEvent, SortableOptions } from 'sortablejs'
+import { isDef } from 'utils'
+import type { AutoScrollOptions } from 'sortablejs/plugins'
 import ProgramStepForm from './ProgramStepForm.vue'
 import { useEditorStore } from '~~/composables/editor'
+import type { ProgramStep } from '~/shared/types'
 
+const emit = defineEmits<{
+  (event: 'stepMove', from: number, to: number): void
+}>()
 const editor = useEditorStore()
+let dragged: any | null = null
+
+const sortableOptions: SortableOptions & AutoScrollOptions = {
+  handle: '.command-drag-handle',
+  group: {
+    name: 'program-editor',
+    pull: false,
+    put: true,
+  },
+}
+
+function onDragStart(event: SortableEvent) {
+  editor.selectedStep = -1
+  if (isDef(event.oldIndex)) {
+    dragged = editor.program?.steps[event.oldIndex] || null
+  }
+}
+
+function onDragEnd(event: SortableEvent) {
+  if (isDef(event.newIndex) && isDef(event.oldIndex) && dragged) {
+    emit('stepMove', event.oldIndex, event.newIndex)
+    dragged = null
+  }
+}
 </script>
 
 <template>
-  <div class="program-editor e-div-y pb-100">
-    <div
-      v-for="(step, index) in editor.program?.steps"
-      :key="step.stepId"
-      @click="editor.changeSelection(index)"
-    >
+  <Sortable
+    class="program-editor e-div-y"
+    :list="// eslint-disable-next-line vue/no-extra-parens
+      (editor.program?.steps as ProgramStep[])"
+    item-key="stepId"
+    :options="sortableOptions"
+    @start="onDragStart"
+    @end="onDragEnd"
+  >
+    <template #item="{ index }: { index: number, element: any }">
       <QItem
         dense
         class="program-step"
         :class="{ __selected: editor.selectedStep === index }"
       >
-        <span class="!text-(white opacity-20)">{{ editor.program?.steps[index].stepId }}</span>
         <QItemSection side class="w-4 pr-4">
           <QItemLabel>
             {{ index + 1 }}
           </QItemLabel>
         </QItemSection>
         <QItemSection class="pl-2">
-          <ProgramStepForm :path="`steps.${index}`" />
+          <div @click="editor.changeSelection(index)">
+            <ProgramStepForm :path="`steps.${index}`" />
+          </div>
         </QItemSection>
-        <div class="absolute top-3 right-3">
-          <QItemSection side>
-            <QBtn
-              class="btn-delete"
-              icon="close"
-              flat
-              dense
-              @click="editor.deleteStep(index)"
-            />
-          </QItemSection>
-          <QItemSection
+
+        <QItemSection side class="relative flex items-center">
+          <QIcon
+            class="icon absolute top-2 cursor-pointer"
+            name="close"
+            @click="editor.deleteStep(index)"
+          />
+          <QIcon
             v-if="editor.isDragging"
-            side
-            class="command-drag-handle"
-          >
-            <QIcon name="drag_handle" />
-          </QItemSection>
-        </div>
+            class="icon command-drag-handle mt-7 cursor-move"
+            name="drag_handle"
+          />
+        </QItemSection>
       </QItem>
-    </div>
-  </div>
+    </template>
+  </Sortable>
 </template>
 
 <style lang="postcss" scoped>
 .program-step {
-  @apply relative;
   @apply hover:(bg-gray-1 dark:bg-dark-1);
   @apply transition-none;
-  @apply pl-2;
 }
 
 .program-step.__selected {
   @apply bg-gray-1 dark:bg-dark-3;
 }
 
-.btn-delete {
+.icon {
   @apply text:(white opacity-60);
   @apply dark:(text-white opacity-60);
-  @apply ml-2;
 }
 </style>
