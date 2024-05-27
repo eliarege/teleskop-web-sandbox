e9fe8a1552d838e53aba2a941e4ec240b4c538bb
Onur Enginer <onur.enginer@eliarge.com>
2024-05-27 11:49:00 +0300

    chore(dms): Changed ping fail response

--- apps/dispensing-management-systems/server/api/ping.post.ts
+++ apps/dispensing-management-systems/server/api/ping.post.ts
@@ -1,7 +1,13 @@
 import { execa } from 'execa'
 
 export default defineEventHandler(async (event) => {
-  const { address } = await readBody(event)
-  const { stdout } = await execa`ping -c 1 ${address}`;
-  return stdout
+  try {
+    const { address } = await readBody(event)
+    const { stdout } = await execa`ping -c 1 ${address}`
+    return stdout
+  }
+  catch(e) {
+    setResponseStatus(event, 404, 'Target Unreachable')
+    event.node.res.end()
+  }
 })
