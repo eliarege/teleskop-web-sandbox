39c3d35acd78b4af3a15477ab1d6bd279e30d474
Mücahit Yaman <mucahit.yaman@eliarge.com>
2024-05-28 14:19:31 +0300

    fix: Select Parameter Input

--- apps/program-editor/components/ProgramStepCommandParameterInput.vue
+++ apps/program-editor/components/ProgramStepCommandParameterInput.vue
@@ -11,42 +11,51 @@ const props = defineProps<{
 const { t } = useI18n()
 const editor = useEditorStore()
 const programParameter: ParameterItem = editor.getPathElement(props.path)
+const model = ref(Number(programParameter.value))
 
 const rules = [
   (value: any) => !!value || t('input.required', { field: t('program.parameter') }),
   (value: any) => (value >= props.parameter.minValue && value <= props.parameter.maxValue) || t('valueOutOfRange', { minValue: props.parameter.minValue, maxValue: props.parameter.maxValue }),
 ]
+
+const options = computed(() => props.parameter.selections || [])
+
+watch(() => model.value, (newValue) => {
+  programParameter.value = newValue
+})
 </script>
 
 <template>
   <template v-if="parameter.type === 'NUMBER'">
     <InputDuration
       v-if="parameter.format === 'DURATION'"
-      v-model="programParameter.value"
+      v-model="model"
       :label="parameter.name"
       :min-value="parameter.minValue"
       :max-value="parameter.maxValue"
     />
     <InputNumber
       v-else
-      v-model="programParameter.value"
+      v-model="model"
       :rules="rules"
       :label="parameter.name.charAt(0).toUpperCase() + parameter.name.slice(1)"
       type="decimal"
       :maxlength="10"
+      :hide-bottom-space="true"
       outlined
       dense
     />
   </template>
   <QSelect
     v-else-if="parameter.type === 'SELECT'"
-    v-model="programParameter.value"
+    v-model="model"
     :label="parameter.name"
-    :options="parameter.selections"
+    :options="options"
     option-label="name"
+    option-value="value"
+    options-dense
     map-options
     emit-value
-    options-dense
     outlined
     dense
     style="width: 150px;"
