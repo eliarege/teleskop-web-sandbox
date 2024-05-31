fdd18c968a37b18d98b63471759ad4cf844b5d59
Ege İliklier <ege.iliklier@eliarge.com>
2024-05-31 17:34:49 +0300

    fix(nuxt-base): head link urls via nitro plugin

---
+++ packages/nuxt-base/server/plugins/head.ts
@@ -0,0 +1,28 @@
+import { createHead } from 'unhead'
+import { renderSSRHead } from '@unhead/ssr'
+import { withBase } from 'ufo'
+
+const darkThemeMedia = '(prefers-color-scheme: dark)'
+const lightThemeMedia = '(prefers-color-scheme: light)'
+
+export default defineNitroPlugin(async (nitro) => {
+  const { baseURL } = useRuntimeConfig().app
+  const head = createHead()
+  head.push({
+    link: [
+      { rel: 'icon', type: 'image/png', media: lightThemeMedia, sizes: '32x32', href: withBase('/favicon-light-32x32.png', baseURL) },
+      { rel: 'icon', type: 'image/png', media: lightThemeMedia, sizes: '16x16', href: withBase('/favicon-light-16x16.png', baseURL) },
+      { rel: 'icon', type: 'image/png', media: darkThemeMedia, sizes: '32x32', href: withBase('/favicon-dark-32x32.png', baseURL) },
+      { rel: 'icon', type: 'image/png', media: darkThemeMedia, sizes: '16x16', href: withBase('/favicon-dark-16x16.png', baseURL) },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: withBase('/apple-touch-icon.png', baseURL) },
+      { rel: 'manifest', href: withBase('/site.webmanifest', baseURL) },
+      { rel: 'mask-icon', href: withBase('/safari-pinned-tab.svg', baseURL), color: '#1c1917' },
+    ],
+  })
+
+  const { headTags } = await renderSSRHead(head)
+
+  nitro.hooks.hook('render:html', (htmlContext) => {
+    htmlContext.head.push(headTags)
+  })
+})
