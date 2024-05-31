a9c94a75bb13bc0d3278ef591de543e8e782050b
Mücahit Yaman <mucahit.yaman@eliarge.com>
2024-05-31 16:21:00 +0300

    feat: Add compatibility level check at the beginning of the program.

--- apps/program-editor/server/plugins/init-db.ts
+++ apps/program-editor/server/plugins/init-db.ts
@@ -1,13 +1,21 @@
+import process from 'node:process'
 import { db } from '../database'
 import { sql } from '../sql'
 
 export default defineNitroPlugin(async () => {
-  const config = useRuntimeConfig()
-  const [{ compatibility_level }] = await db.raw(sql`
+  try {
+    const config = useRuntimeConfig()
+    const [{ compatibility_level }] = await db.raw(sql`
     SELECT compatibility_level
     FROM sys.databases WHERE name = ?`, [config.teleskopDatabase])
 
-  if (compatibility_level < 130) {
-    await db.raw(sql`ALTER DATABASE ${config.teleskopDatabase} SET COMPATIBILITY_LEVEL = 130`)
+    if (compatibility_level < 130) {
+      await db.raw(sql`ALTER DATABASE [${config.teleskopDatabase}] SET COMPATIBILITY_LEVEL = 130`)
+    }
+  } catch (e) {
+    console.error('Failed to set COMPATIBILITY_LEVEL', e)
+    if (!import.meta.dev) {
+      process.exit(1)
+    }
   }
 })
