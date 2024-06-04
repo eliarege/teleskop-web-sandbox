65322936f33d96f917dde38649a1c15c4ae5ce62
Ege İliklier <ege.iliklier@eliarge.com>
2024-06-04 21:53:11 +0300

    chore(planning-board-engine): use `defineConfiguration`

---
+++ apps/planning-board-engine/src/config.ts
@@ -0,0 +1,37 @@
+import { defineConfiguration } from 'utils'
+
+export const config = defineConfiguration({
+  teleskopHost: {
+    env: 'TELESKOP_HOST',
+    required: true,
+  },
+  teleskopPort: {
+    env: 'TELESKOP_PORT',
+    type: 'number',
+    default: 1433,
+  },
+  teleskopUser: {
+    env: 'TELESKOP_USER',
+    required: true,
+  },
+  teleskopPassword: {
+    env: 'TELESKOP_PASSWORD',
+    required: true,
+  },
+  teleskopDatabase: {
+    env: 'TELESKOP_DATABASE',
+    required: true,
+  },
+  teleskopInstanceName: {
+    env: 'TELESKOP_INSTANCE_NAME',
+  },
+  /**
+   * Teleskop veri tabanında tutulan `datetime` değerleri genellikle teleskop uygulamalarının çalıştığı sistemin saatine göre kaydedilmektedir.
+   * Burdaki sayı değeri, bu farkı ortadan kaldırmak için kullanılmaktadır.
+   */
+  teleskopTimezoneOffset: {
+    env: 'TELESKOP_TIMEZONE_OFFSET',
+    type: 'integer',
+    default: -180,
+  },
+})
--- apps/planning-board-engine/src/knexConfig.ts
+++ apps/planning-board-engine/src/knexConfig.ts
@@ -1,15 +1,19 @@
-import process from 'node:process'
 import Knex from 'knex'
+import { config } from './config'
 
 export const knex = Knex({
   client: 'mssql',
   connection: {
-    server: process.env.TELESKOP_HOST,
-    // host: process.env.teleskopHost,
-    port: Number.parseInt(process.env.TELESKOP_PORT || '1433'),
-    user: process.env.TELESKOP_USER,
-    password: process.env.TELESKOP_PASSWORD,
-    database: process.env.TELESKOP_DATABASE,
+    server: config.teleskopHost,
+    ...(
+      config.teleskopInstanceName
+        ? { instanceName: config.teleskopInstanceName }
+        : { port: config.teleskopPort }
+    ),
+    user: config.teleskopUser,
+    password: config.teleskopPassword,
+    database: config.teleskopDatabase,
+    connectTimeout: 30_000,
     options: {
       trustServerCertificate: true,
     },
