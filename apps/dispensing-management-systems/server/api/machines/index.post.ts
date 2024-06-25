0b7f311f20eb24ad719655c50f5f1dd56979f9ea
Onur Enginer <onur.enginer@eliarge.com>
2024-06-25 14:11:52 +0300

    fix(dms): Duplicate primary key handle

--- apps/dispensing-management-systems/server/api/machines/index.post.ts
+++ apps/dispensing-management-systems/server/api/machines/index.post.ts
@@ -1,12 +1,20 @@
 import { dmsDB } from '~/server/connectionPool'
 import type { Machine } from '~/shared/types'
+import { ErrorCodes } from '~/shared/constants'
 
 export default defineEventHandler(async (event) => {
   const machine: Machine = await readBody(event)
-  const res = await dmsDB('MACHINE').insert({
-    machine_id: machine.machineId,
-    machine_name: machine.machineName,
-    controller_type: machine.controllerType,
-  })
-  return res
+  try {
+    const res = await dmsDB('MACHINE').insert({
+      machine_id: machine.machineId,
+      machine_name: machine.machineName,
+      controller_type: machine.controllerType
+    })
+    return res
+  } catch (e: any) {
+    if (e.code === ErrorCodes.unique) {
+      setResponseStatus(event, 403, 'A Machine with that ID already exists.')
+      event.node.res.end()
+    }
+  }
 })
--- apps/dispensing-management-systems/server/api/materials/index.post.ts
+++ apps/dispensing-management-systems/server/api/materials/index.post.ts
@@ -1,20 +1,28 @@
 import { dmsDB } from '~/server/connectionPool'
+import { ErrorCodes } from '~/shared/constants'
 import type { Material } from '~/shared/types'
 
 export default defineEventHandler(async (event) => {
   const material: Material = await readBody(event)
-  const res = await dmsDB('MATERIAL')
-    .insert({
-      material_code: material.materialCode,
-      material_name: material.materialName,
-      material_group_no: material.materialGroupNo,
-      density: material.density,
-      ph: material.ph,
-      source: material.source,
-      cost_unit: material.costUnit,
-      unit_cost: material.unitCost,
-      re_requestable: material.reRequestable,
-      direct_transfer: material.directTransfer,
-    })
-  return res
+  try {
+    const res = await dmsDB('MATERIAL')
+      .insert({
+        material_code: material.materialCode,
+        material_name: material.materialName,
+        material_group_no: material.materialGroupNo,
+        density: material.density,
+        ph: material.ph,
+        source: material.source,
+        cost_unit: material.costUnit,
+        unit_cost: material.unitCost,
+        re_requestable: material.reRequestable,
+        direct_transfer: material.directTransfer,
+      })
+    return res
+  } catch (e: any) {
+    if (e.code === ErrorCodes.unique) {
+      setResponseStatus(event, 403, 'A Material with that code already exists.')
+      event.node.res.end()
+    }
+  }
 })
