c06bb4b83988034731104978cdce1b1a00428131
Onur Enginer <onur.enginer@eliarge.com>
2024-07-04 14:56:49 +0300

    fix(dms): Locale setting changes

--- apps/dispensing-management-systems/app.vue
+++ apps/dispensing-management-systems/app.vue
@@ -1,15 +1,10 @@
 <script lang ="ts" setup>
-import languageTR from 'quasar/lang/tr'
 import { useStateStore } from '~/store/State'
 
-const q = useQuasar()
 const state = useStateStore()
 const { setLocale } = useI18n()
 
 setLocale(state.locale)
-
-if (state.locale === 'tr')
-  q.lang.set(languageTR)
 </script>
 
 <template>
