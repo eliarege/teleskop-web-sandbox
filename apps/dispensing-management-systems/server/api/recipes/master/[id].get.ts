70fcadae1ed0420f49b386f8eb91343b622ba1e3
Onur Enginer <onur.enginer@eliarge.com>
2024-07-04 15:26:55 +0300

    fix(dms): Missing field

--- apps/dispensing-management-systems/server/api/recipes/master/[id].get.ts
+++ apps/dispensing-management-systems/server/api/recipes/master/[id].get.ts
@@ -4,6 +4,7 @@ import type { RecipeMaster } from '~/shared/types'
 export default defineEventHandler(async (event) => {
   const { id } = getRouterParams(event)
   const recipe: RecipeMaster = await dmsDB('RECIPE_MASTER').select({
+    recipeId: 'recipe_id',
     recipeName: 'recipe_name',
     recipeGroup: 'recipe_group',
     recipeType: 'recipe_type',
