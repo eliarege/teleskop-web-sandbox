c674ff0d756a038883f764f71b7c912ca316c841
Ege İliklier <ege.iliklier@eliarge.com>
2024-07-25 18:54:55 +0300

    refactor(multi-monitor): moved consumption trends to `ConsumptionItem` component

--- apps/multi-monitor/components/NavBar/Consumption.vue
+++ apps/multi-monitor/components/NavBar/Consumption.vue
@@ -16,107 +16,43 @@ const { data: trends } = useFetch('/api/trends', {
   }),
 })
 const { t } = useI18n()
-
 const store = useDataStore()
-
-const textGreen = 'text-green-500'
-const textRed = 'text-red-500'
-
-function textFormatter(text: number) {
-  return Math.round(text)
-    .toString()
-    .replace(/(\d)(?=(\d{3})+$)/g, '$1' + '.')
-}
 </script>
 
 <template>
   <div class="flex items-center justify-center gap-15 w-full h-full">
-    <div v-if="store.electricity" class="icons">
-      <QTooltip
-        transition-show="scale"
-        class="text-black e-border bg-white"
-        :offset="[3, 3]"
-      >
-        {{ t("teleskop.consumption-electricity") }}
-        {{ textFormatter(trends.lastWeekElectricity) }}
-      </QTooltip>
-      <Icon
-        name="ant-design:thunderbolt-twotone"
-        size="40"
-        color="#2281ae"
-      />
-      <span
-        :class="trends.currentWeekElectricity > trends.lastWeekElectricity ? textRed : textGreen "
-      >
-        {{ textFormatter(trends.currentWeekElectricity) }} KWH
-      </span>
-    </div>
-    <div
+    <ConsumptionItem
+      v-if="store.electricity"
+      :label="t('teleskop.consumption-electricity')"
+      icon="ant-design:thunderbolt-twotone"
+      unit="kWh"
+      :previous-value="trends.lastWeekElectricity"
+      :current-value="trends.currentWeekElectricity"
+    />
+    <ConsumptionItem
       v-if="store.water"
-      id="water"
-      class="icons"
-    >
-      <QTooltip
-        transition-show="scale"
-        class="text-black e-border bg-white"
-        :offset="[3, 3]"
-      >
-        {{ t("teleskop.consumption-water") }}
-        {{ textFormatter(trends.lastWeekTotalWater) }}
-      </QTooltip>
-      <Icon
-        name="ic:baseline-water-drop"
-        color="#2281ae"
-        size="40"
-        :horizontal-flip="true"
-      />
-      <span
-        :class="trends.currentWeekTotalWater > trends.lastWeekTotalWater ? textRed : textGreen "
-      >
-        {{ textFormatter(trends.currentWeekTotalWater) }} L
-      </span>
-    </div>
-    <div v-if="store.salt" class="icons">
-      <QTooltip
-        transition-show="scale"
-        class="text-black e-border bg-white"
-        :offset="[3, 3]"
-      >
-        {{ t("teleskop.consumption-salt") }}
-        {{ textFormatter(trends.lastWeekSalt) }}
-      </QTooltip>
-      <Icon
-        name="tabler:salt"
-        size="40"
-        color="#2281ae"
-      />
-      <span
-        :class="trends.currentWeekSalt > trends.lastWeekSalt ? textRed : textGreen "
-      >
-        {{ textFormatter(trends.currentWeekSalt) }} L
-      </span>
-    </div>
-    <div v-if="store.steam" class="icons">
-      <QTooltip
-        transition-show="scale"
-        class="text-black e-border bg-white"
-        :offset="[3, 3]"
-      >
-        {{ t("teleskop.consumption-steam") }}
-        {{ textFormatter(trends.lastWeekSteam) }}
-      </QTooltip>
-
-      <Icon
-        name="fluent:building-factory-48-regular"
-        size="40"
-        color="#2281ae"
-      />
-      <span
-        :class="trends.currentWeekSteam > trends.lastWeekSteam ? textRed : textGreen "
-      >
-        {{ textFormatter(trends.currentWeekSteam) }} KG
-      </span>
-    </div>
+      :label="t('teleskop.consumption-water')"
+      icon="ic:baseline-water-drop"
+      unit="L"
+      :previous-value="trends.lastWeekTotalWater"
+      :current-value="trends.currentWeekTotalWater"
+    />
+    <ConsumptionItem
+      v-if="store.salt"
+      :label="t('teleskop.consumption-salt')"
+      icon="tabler:salt"
+      unit="L"
+      :previous-value="trends.lastWeekSalt"
+      :current-value="trends.currentWeekSalt"
+    />
+    <ConsumptionItem
+      v-if="store.steam"
+      :label="t('teleskop.consumption-steam')"
+      icon="fluent:building-factory-48-regular"
+      unit="kg"
+      :previous-value="trends.lastWeekSteam"
+      :current-value="trends.currentWeekSteam"
+    />
   </div>
 </template>
 
