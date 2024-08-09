bb0e5812ad91e097f9c37140d98c9be572712841
Ege İliklier <ege.iliklier@eliarge.com>
2024-08-09 22:20:03 +0300

    chore(ui): limited exports, export `.ts` file

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,3 +1,7 @@
+/// <reference path="./types/novnc.d.ts" />
+
+export type { NoVncProps } from './components/NoVnc.vue'
+
 export { default as NoVnc } from './components/NoVnc.vue'
 export { default as LoadingSpinner } from './components/LoadingSpinner.vue'
 export { default as LoadingScreen } from './components/LoadingScreen.vue'
