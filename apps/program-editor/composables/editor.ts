eliarege/teleskop-web-sandbox

commit 03d0272a5c6d81ba52698a17543a404bb71fc57c

refactor(PE): move updateTeleskopSettings to editor store

apps/program-editor/composables/editor.ts
@@ -473,6 +473,19 @@ export const useEditorStore = defineStore('editor', () => {
     teleskopSettings.value = await fetch<TeleskopSettings[]>(`/api/teleskop-settings`)
   }
 
+  async function updateTeleskopSettings(id: number, value: string) {
+    await fetch('/api/teleskop-settings', {
+      method: 'PUT',
+      body: { id, value },
+    })
+    const setting = teleskopSettings.value.find(st => st.id === id)
+    if (setting) {
+      setting!.value = value
+    } else {
+      teleskopSettings.value.push({ id, value })
+    }
+  }
+
   async function fetchCommandTypes(machineId: number) {
     machine.value.commandTypes = await fetch<CommandTypes[]>(`/api/machine/${machineId}/command-types`)
   }
@@ -524,6 +537,7 @@ export const useEditorStore = defineStore('editor', () => {
     rightDrawerOpen,
     theoricDuration,
     treatmentSettings,
+    teleskopSettings,
     changeMachine,
     fetchProgram,
     fetchMachine,
@@ -553,7 +567,7 @@ export const useEditorStore = defineStore('editor', () => {
     scrollPage,
     getStepIcon,
     fetchTeleskopSettings,
-    teleskopSettings,
+    updateTeleskopSettings,
     fetchCommandTypes,
   }
 })

apps/program-editor/composables/new.commands.ts
@@ -356,7 +356,7 @@ registerCommand(() => {
 })
 
 registerCommand(() => {
-  const { fetch } = useKeycloak()
+  const editor = useEditorStore()
   return {
     name: 'editProgramIcons',
     async execute(ctx: any) {
@@ -364,7 +364,7 @@ registerCommand(() => {
         component: TBApplicationSettingsDialog,
       }).onOk(async (value: string) => {
         // Number 12 for icon settings
-        await setTeleskopSettings(12, value)
+        await editor.updateTeleskopSettings(12, value)
       })
       return true
     },

apps/program-editor/composables/utils.ts
@@ -287,22 +287,3 @@ export function parseDuration(duration: string): number {
   }
   return Number(duration) * 36000
 }
-
-export async function setTeleskopSettings(id: number, value: string): Promise<void> {
-  await $fetch(`/api/teleskop-settings`, {
-    method: 'PUT',
-    body: {
-      id,
-      value,
-    },
-  })
-  const editor = useEditorStore()
-  await editor.fetchTeleskopSettings()
-}
-
-export async function getTeleskopSettings(): Promise<string> {
-  const { data } = await $fetch('/api/teleskop-settings', {
-    method: 'GET',
-  })
-  return data
-}

commit 391951a781e4630ef8b6a0d104151e5fe7ed9d7f

fix(PE): regression caused by 03d0272a

apps/program-editor/composables/editor.ts
@@ -27,7 +27,8 @@ export const useEditorStore = defineStore('editor', () => {
   let lastStepId = 0
   let lastCommandId = 0
 
-  const { t } = useI18n()
+  const { $i18n } = useNuxtApp()
+  const { t } = $i18n
   const route = useRoute()
   const errorIds = ref(new Set<string>())
   const { notifySuccess, notifyError } = useNotify()

apps/program-editor/composables/new.commands.ts
@@ -356,10 +356,10 @@ registerCommand(() => {
 })
 
 registerCommand(() => {
-  const editor = useEditorStore()
   return {
     name: 'editProgramIcons',
     async execute(ctx: any) {
+      const editor = useEditorStore()
       ctx.$q.dialog({
         component: TBApplicationSettingsDialog,
       }).onOk(async (value: string) => {