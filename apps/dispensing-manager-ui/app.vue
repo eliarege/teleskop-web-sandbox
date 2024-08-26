4a46c13bbf4c508d37b96ae056ef8dfe24131ce4
Alper Atmaca <alper.atmaca@eliarge.com>
2024-08-26 15:32:54 +0300

    chore(DY): fixed scrollbar

--- apps/dispensing-manager-ui/app.vue
+++ apps/dispensing-manager-ui/app.vue
@@ -9,25 +9,32 @@
 
 <style>
 .q-field--filled .q-field__control {
-    border-radius: 4px 4px 0 0;
-    background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 4px 4px 0 0;
+  background-color: rgba(0, 0, 0, 0.2);
 }
 
 .q-radio__inner--truthy {
-    color: #000000;
+  color: #000000;
 }
-.q-checkbox__inner--truthy, .q-checkbox__inner--indet {
-    color: #000000;
+.q-checkbox__inner--truthy,
+.q-checkbox__inner--indet {
+  color: #000000;
+}
+
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  background-color: #f5f5f5;
 }
 
 ::-webkit-scrollbar {
   width: 0.55rem;
-  height: 100%;
+  height: 0.55rem;
+  background-color: #f5f5f5;
 }
 
 ::-webkit-scrollbar-thumb {
-  background-color: #000000;
-  opacity: 50%;
   border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  background-color: #000000;
 }
 </style>
