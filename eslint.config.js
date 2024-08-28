a0e3c284ca4a5d35d1eed06257dd944337666b5c
Ege İliklier <ege.iliklier@eliarge.com>
2024-08-28 15:34:47 +0300

    feat: added schedulerpro@6.0.4

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,6 @@ import { workspaceEslintConfig } from '@teleskop/eslint-config'
 export default workspaceEslintConfig.append({
   ignores: [
     'packages/tbb-ftp-client/tests/fixtures/data',
+    'vendor',
   ],
 })
