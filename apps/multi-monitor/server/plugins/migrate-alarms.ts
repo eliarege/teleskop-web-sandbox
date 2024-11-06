549e0833045ab756bd0446bb60fe5a3b44659d89
Ege İliklier <ege.iliklier@eliarge.com>
2024-11-06 12:08:42 +0300

    refactor(MM): alarms server plugin
    Added description, shortened code, and renamed the plugin filename. Also removed .then/.catch blocks, as previous code continued executing due to unrecoverable errors being caught.

---
+++ apps/multi-monitor/server/plugins/migrate-alarms.ts
@@ -0,0 +1,28 @@
+import { knex } from '../knexConfig'
+
+// On server start-up, this plugin adds SHOWONSCREEN column to BFMASTERCOMMANDSALARMS table if it doesnt exist.
+export default defineNitroPlugin(async () => {
+  try {
+    const alarmsHasShowColumn = await knex.schema.hasColumn('BFMASTERCOMMANDSALARMS', 'SHOWONSCREEN')
+
+    if (!alarmsHasShowColumn) {
+      await knex.schema.alterTable('BFMASTERCOMMANDSALARMS', (table) => {
+        table.boolean('SHOWONSCREEN').defaultTo(true)
+      })
+      console.log('Column SHOWONSCREEN created successfully!')
+    } else {
+      // Synchronize
+      await knex.schema.alterTable('BFMASTERCOMMANDSALARMS', (table) => {
+        table.boolean('SHOWONSCREEN').alter().defaultTo(true)
+      })
+    }
+
+    await knex('BFMASTERCOMMANDSALARMS')
+      .update({ SHOWONSCREEN: true })
+      .where('SHOWONSCREEN', null)
+
+    console.log('Existing records updated successfully!')
+  } catch (err) {
+    console.error('Failed to migrate BFMASTERCOMMANDSALARMS')
+  }
+})
