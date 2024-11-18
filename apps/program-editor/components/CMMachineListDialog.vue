8850a21b9380a72c92aeb7ab3a5650c90210d46a
Mücahit Yaman <mucahit.yaman@eliarge.com>
2024-11-18 17:02:25 +0300

    feat(PE): improved machine selection dialog
    Enhanced the appearance of the machine selection dialog, added expand/collapse button, and included language support.

--- apps/program-editor/components/CMMachineListDialog.vue
+++ apps/program-editor/components/CMMachineListDialog.vue
@@ -18,6 +18,7 @@ const { data: allMachine } = useAuthFetch<MachineInfo[]>('/api/machine')
 
 const ticked = ref<string[]>([])
 const selectAll = ref(false)
+const expanded = ref<string[]>([])
 
 const nodes = computed(() => {
   if (!machineGroup.value || !allMachine.value)
@@ -41,36 +42,71 @@ function selectAllMachines() {
   else
     ticked.value = []
 }
+function expandToogle() {
+  if (expanded.value.length === 0)
+    expanded.value = nodes.value.flatMap(getAllNodeIds)
+  else
+    expanded.value = []
+}
+
+function getAllNodeIds(node: any) {
+  const ids = [node.id]
+  if (node.children) {
+    ids.push(...node.children.flatMap(getAllNodeIds))
+  }
+  return ids
+}
 </script>
 
 <template>
-  <q-dialog ref="dialogRef" persistent>
+  <q-dialog ref="dialogRef">
     <q-card>
-      <q-card-section class="bg-gray-1">
-        <div>
-          {{ t(`contextMenu.${props.type}.selectMachine`) }}
+      <q-card-section class="w-100">
+        <div class="text-h6 flex">
+          {{ t('contextMenu.copyToMachinesAndSend') }}
+          <q-space />
+          <q-btn
+            icon="close"
+            flat
+            round
+            dense
+            @click="onDialogCancel"
+          />
         </div>
       </q-card-section>
       <q-card-section>
-        <div class="pb-3">
-          <QCheckbox
-            v-model="selectAll"
-            :label="selectAll ? t('dropAll') : t('selectAll')"
-            dense
-            @update:model-value="selectAllMachines"
-          />
+        <div class="text-sm mb-2">
+          {{ t(`contextMenu.${props.type}.selectMachine`) }}
         </div>
-        <div class="flex justify-center ">
+        <div class="flex justify-center">
           <QTree
             v-model:ticked="ticked"
+            v-model:expanded="expanded"
             :nodes="nodes"
             node-key="id"
             tick-strategy="leaf"
             default-expand-all
             dense
+            class="w-full min-h-120 max-h-120 overflow-y-scroll"
           />
         </div>
       </q-card-section>
+      <div class="q-pa-md">
+        <QCheckbox
+          v-model="selectAll"
+          class="w-34"
+          :label="selectAll ? t('dropAll') : t('selectAll')"
+          dense
+          @update:model-value="selectAllMachines"
+        />
+        <q-btn
+          :label="expanded.length ? t('collapseAll') : t('expandAll')"
+          :icon="expanded.length ? 'expand_less' : 'expand_more'"
+          color="primary"
+          dense
+          @click="expandToogle"
+        />
+      </div>
 
       <q-card-actions
         align="right"
