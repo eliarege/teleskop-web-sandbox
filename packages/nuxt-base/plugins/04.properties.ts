d8bd124130f4f14cd2f8ee4747cdec11234bde26
Alper Atmaca <alper.atmaca@eliarge.com>
2024-12-26 16:55:09 +0300

    fix(nuxt-base): changed theme default & added app based themes

--- packages/nuxt-base/plugins/04.properties.ts
+++ packages/nuxt-base/plugins/04.properties.ts
@@ -1,7 +1,13 @@
+import { useStorage } from '@vueuse/core'
 import type { AppProperties } from '~/types'
 
 export default defineNuxtPlugin(async () => {
+  const { dark } = useQuasar()
   const appProps = await $fetch<AppProperties>('/api/properties')
+
+  const darkMode = useStorage<'auto' | boolean>(`${appProps.name}.theme`, false, localStorage)
+  dark.set(darkMode.value)
+
   return {
     provide: {
       appProps,
--- packages/nuxt-base/plugins/06.theme.client.ts
+++
@@ -1,7 +0,0 @@
-import { useStorage } from '@vueuse/core'
-
-export default defineNuxtPlugin(() => {
-  const { dark } = useQuasar()
-  const darkMode = useStorage<'auto' | boolean>('theme', 'auto', localStorage)
-  dark.set(darkMode.value)
-})
