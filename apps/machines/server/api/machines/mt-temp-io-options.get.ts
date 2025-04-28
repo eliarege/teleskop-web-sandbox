f31a966f5144c5df2533e6f2f3cbb5d0aa7f2e66
mucahityaman <mucahit.yaman@eliarge.com>
2025-04-28 17:03:21 +0300

    fix(MA): fixed table name

--- apps/machines/server/api/machines/mt-temp-io-options.get.ts
+++ apps/machines/server/api/machines/mt-temp-io-options.get.ts
@@ -1,7 +1,7 @@
 import { knex } from '~/server/connectionPool'
 
 export default defineAuthEventHandler(async () => {
-  const MTTempIOOptions: { machineId: number, id: number, name: string }[] = await knex('BFMACHDOUT')
+  const MTTempIOOptions: { machineId: number, id: number, name: string }[] = await knex('BFMACHAIN')
     .select({
       machineId: 'MACHINEID',
       id: 'ID',
