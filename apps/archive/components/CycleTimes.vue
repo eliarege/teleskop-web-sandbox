0651393a72220523f89c901104e59fbfce93d5d5
Hasan Can Erol <hasancan.erol@eliarge.com>
2025-05-13 11:50:28 +0300

    feat(AR): cycle count  at selected time added to cycle line

--- apps/archive/components/CycleTimes.vue
+++ apps/archive/components/CycleTimes.vue
@@ -4,21 +4,36 @@ import type { AnalogInputOutputType, IOSetting, Reel } from '~/types/archive'
 import { userSettingsStore } from '~/composables/userSettingsStore'
 import { getCommandsWithClosestTime } from '~/utils/functions'
 
-defineProps<{
+const props = defineProps<{
   cycles: Reel[]
   selectedTime: Date
 }>()
 
 const settingsStore = userSettingsStore()
 const { t } = useI18n()
-// const commandsWithClosestTime = computed(() => {
-//   return getCommandsWithClosestTime(props.selectedTime, props.commands)
-// })
+const cyclesWithClosestCycleCount = computed(() => {
+  const selectedTime = props.selectedTime.getTime()
+
+  return props.cycles.map((cycle) => {
+    const closestCount = cycle?.cycles?.length > 0
+      ? cycle.cycles.reduce((closest, current) => {
+        const currentTime = new Date(current.cycledAt).getTime()
+        const closestTime = new Date(closest.cycledAt).getTime()
+        const currentDiff = Math.abs(currentTime - selectedTime)
+        const closestDiff = Math.abs(closestTime - selectedTime)
+
+        return currentDiff < closestDiff ? current : closest
+      })
+      : { cycledAt: '', count: 0 }
+
+    return { ...cycle, closestCount }
+  })
+})
 </script>
 
 <template>
   <div class="py-2 overflow-y-auto h-full space-y-0.5 divide-y-1 divide-gray-300">
-    <div v-for="cycle in cycles" :key="`${cycle.reelNo}reelNo`">
+    <div v-for="cycle in cyclesWithClosestCycleCount" :key="`${cycle.reelNo}reelNo`">
       <div class="flex items-center flex-nowrap mb-1">
         <q-checkbox
           :model-value="settingsStore.getSetting(`cycleTimes_${cycle.reelNo}`).selected"
@@ -58,6 +73,9 @@ const { t } = useI18n()
           {{ `${cycle.reelNo + 1}. ${t("reel")}` }}
         </span>
         <q-space />
+        <span class="mr-4">
+          {{ cycle.closestCount.count + 1 }}
+        </span>
       </div>
     </div>
   </div>
