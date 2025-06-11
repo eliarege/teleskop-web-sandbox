7929360d0472d7d526493bb403b0c9e51a81ff13
Ege İliklier <ege.iliklier@eliarge.com>
2025-06-11 14:43:26 +0300

    feat(nuxt-base): added more customization options to LoadingSpinner

--- packages/nuxt-base/components/LoadingSpinner.vue
+++ packages/nuxt-base/components/LoadingSpinner.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
 import '../assets/animations.css'
 
-withDefaults(defineProps<{ withBackground?: boolean }>(), {
+withDefaults(defineProps<{
+  outerColor?: string
+  centerColor?: string
+  innerColor?: string
+  backgroundColor?: string
+  withBackground?: boolean
+  withWrapper?: boolean
+}>(), {
+  outerColor: '#212121',
+  centerColor: '#0d94fc',
+  innerColor: '#212121',
+  backgroundColor: '#3c3c3c4d',
   withBackground: false,
+  withWrapper: true,
 })
 </script>
 
 <template>
   <div
+    v-if="withWrapper"
     class="spinner-wrapper"
     :class="{ withBackground: 'spinner-wrapper-background' }"
   >
     <span class="spinner" />
   </div>
+  <span v-else class="spinner" />
 </template>
 
 <style scoped lang="postcss">
@@ -25,11 +39,14 @@ withDefaults(defineProps<{ withBackground?: boolean }>(), {
   justify-content: center;
   align-items: center;
 }
+
 .spinner-wrapper-background {
-  background-color: rgba(60, 60, 60, 0.3);
+  background-color: v-bind(backgroundColor);
 }
+
+/* Outer spinner */
 .spinner {
-  border-color: #212121 #212121 transparent transparent;
+  border-color: v-bind(outerColor) v-bind(outerColor) transparent transparent;
   border-width: 3px;
   border-radius: 50%;
   animation: rotation 1s linear infinite;
@@ -42,11 +59,9 @@ withDefaults(defineProps<{ withBackground?: boolean }>(), {
 
 .spinner::after,
 .spinner::before {
-  border-color: transparent transparent #0d94fc #0d94fc;
   border-style: solid;
   border-width: 3px;
   border-radius: 50%;
-  animation: rotation 0.5s reverse infinite;
   transform-origin: center center;
   content: '';
   position: absolute;
@@ -56,12 +71,19 @@ withDefaults(defineProps<{ withBackground?: boolean }>(), {
   bottom: 0;
   margin: auto;
   box-sizing: border-box;
+}
+
+/* Center spinner */
+.spinner::after {
+  border-color: transparent transparent v-bind(centerColor) v-bind(centerColor);
+  animation: rotation 0.5s linear reverse infinite;
   width: 40px;
   height: 40px;
 }
 
+/* Inner spinner */
 .spinner::before {
-  border-color: #212121 #212121 transparent transparent;
+  border-color: v-bind(innerColor) v-bind(innerColor) transparent transparent;
   animation: rotation 1.5s linear infinite;
   width: 32px;
   height: 32px;
