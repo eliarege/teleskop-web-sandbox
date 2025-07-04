db03614604246f61f8626d4f0570b2d4a407217d
mucahityaman <mucahit.yaman@eliarge.com>
2025-07-04 16:09:54 +0300

    refactor(PE): remove method check and add .post call

---
+++ apps/program-editor/server/api/copy.post.ts
@@ -0,0 +1,54 @@
+import { machineStore } from '../classes/MachineStore'
+import { PError } from '../error'
+import type { CopyItem } from '~/shared/types'
+
+export default defineAuthEventHandler(async (event) => {
+  checkPermission(event, 'program-edit')
+  const body = await readBody(event)
+
+  const copyProgram: CopyItem = body.copyProgram
+  const conflicts: CopyItem = { formMachineId: copyProgram.formMachineId, toMachineId: copyProgram.toMachineId, program: [] }
+
+  const fromMachine = await machineStore.get(copyProgram.formMachineId)
+
+  if (!fromMachine) {
+    throw new PError('MACHINE_NOT_FOUND', { machineId: copyProgram.formMachineId })
+  }
+
+  const toMachine = (copyProgram.formMachineId === copyProgram.toMachineId)
+    ? fromMachine
+    : await machineStore.get(copyProgram.toMachineId)
+
+  if (!toMachine) {
+    throw new PError('MACHINE_NOT_FOUND', { machineId: copyProgram.toMachineId })
+  }
+
+  for (const program of copyProgram.program) {
+    const isFromExist = await fromMachine.hasProgram(program.programNo)
+
+    if (!isFromExist) {
+      throw new PError('PROGRAM_NOT_FOUND', { machineId: copyProgram.formMachineId, programNo: program.programNo })
+    }
+
+    const targetProgramNo = program.newProgramNo ?? program.programNo
+    const isToExist = await toMachine.hasProgram(targetProgramNo)
+
+    if (isToExist) {
+      conflicts.program.push({
+        programNo: program.programNo,
+        name: program.name,
+        newProgramNo: null,
+      })
+    } else {
+      const { program: fetchedProgram } = await fromMachine.fetchProgram(program.programNo)
+
+      if (program.newProgramNo) {
+        fetchedProgram.programNo = program.newProgramNo
+      }
+
+      await toMachine.insertProgram(fetchedProgram)
+    }
+  }
+
+  return conflicts
+})
