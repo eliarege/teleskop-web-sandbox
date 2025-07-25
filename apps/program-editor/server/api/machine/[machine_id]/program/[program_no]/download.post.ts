ee71aabf416837f7a64f982af208bba5f115506a
mucahityaman <mucahit.yaman@eliarge.com>
2025-07-25 14:20:42 +0300

    feat(PE): add data check to download endpoint

--- apps/program-editor/server/api/machine/[machine_id]/program/[program_no]/download.post.ts
+++ apps/program-editor/server/api/machine/[machine_id]/program/[program_no]/download.post.ts
@@ -1,9 +1,19 @@
 import { machineStore } from '~/server/classes/MachineStore'
+import { PError } from '~/server/error'
 
 export default defineAuthEventHandler(async (event) => {
   const { machine_id, program_no } = getRouterParams(event)
   const machineId = Number.parseInt(machine_id)
   const programNo = Number.parseInt(program_no)
+
+  if (Number.isNaN(machineId) || Number.isNaN(programNo)) {
+    throw new PError('INVALID_MACHINE_OR_PROGRAM_NUMBER', { machineId, programNo })
+  }
+
   const machine = await machineStore.get(machineId)
+  if (!machine) {
+    throw new PError('MACHINE_NOT_FOUND', { machineId })
+  }
+
   return await machine.fetchRemoteProgram(programNo)
 })
