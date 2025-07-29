c656dd582362528f762fbba2f4ae269c9746e5f1
mucahityaman <mucahit.yaman@eliarge.com>
2025-07-29 17:48:20 +0300

    feat(PE): add program existence check and new dialog component for handling fetch

---
+++ apps/program-editor/components/CMProgramExistsDialog.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import type { ProgramHeader } from '~/shared/types'
+
+const props = defineProps<{
+  program: ProgramHeader
+}>()
+
+const { t } = useI18n()
+const { dialogRef, onDialogOK, onDialogCancel } = useDialogPluginComponent()
+</script>
+
+<template>
+  <QDialog ref="dialogRef" class="select-none">
+    <QCard>
+      <!-- Başlık -->
+      <QCardSection>
+        <div class="text-h6 flex items-center">
+          {{ t(`programExist.title`) }}
+          <QSpace />
+          <QBtn
+            icon="close"
+            class="text-gray-4 dark:text-gray-6"
+            flat
+            round
+            dense
+            @click="onDialogCancel"
+          />
+        </div>
+      </QCardSection>
+
+      <!-- İçerik -->
+      <QCardSection class="text-gray-8 dark:text-gray-3">
+        <span class="max-w-100"> {{ t(`programExist.message`, { programNo: props.program.programNo }) }}</span>
+      </QCardSection>
+
+      <!-- Aksiyonlar -->
+      <QCardActions
+        class="q-pa-md bg-gray-1 dark:bg-dark-4"
+        align="right"
+      >
+        <QBtn
+          :label="t('no')"
+          class="q-mr-sm bg-gray-2 dark:bg-dark-3 text-dark-4 dark:text-gray-2"
+          flat
+          @click="onDialogCancel"
+        />
+        <QBtn
+          class="q-mr-sm text-white bg-primary"
+          :label="t('yes')"
+          flat
+          @click="onDialogOK"
+        />
+      </QCardActions>
+    </QCard>
+  </QDialog>
+</template>
