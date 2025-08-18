8100d82f622a14424fb7d3c6048aacc28c208411
Alper Atmaca <alper.atmaca@eliarge.com>
2025-08-18 14:07:30 +0300

    chore(MA): updated machine fetch query to include mainTank and steamValve queries

--- apps/machines/server/api/machines/machine.put.ts
+++ apps/machines/server/api/machines/machine.put.ts
@@ -2,38 +2,37 @@ import { knex } from '~/server/connectionPool'
 import type { Machine } from '~/types'
 
 export default defineAuthEventHandler(async (event) => {
-  const { machine, machineId } = await readBody<{ machine: Machine, machineId: number }>(event)
+  const { formData } = await readBody<{ formData: Machine }>(event)
 
   const res = await knex('BFMACHINES').where({
-    MACHINEID: machineId,
+    MACHINEID: formData.machineId,
   }).update({
-    MACHINEID: machine.machineId,
-    MACHINECODE: machine.machineCode,
-    GRUPNO: machine.groupId,
-    TBBMODEL: machine.tbbModel,
-    THEORICALCHARGE: machine.theoricalCharge,
-    MACHINECAPACITY: machine.machineCapacity,
-    PlcModel: machine.plcModel,
-    IP: machine.ip,
+    MACHINEID: formData.machineId,
+    MACHINECODE: formData.machineCode,
+    GRUPNO: formData.groupId,
+    TBBMODEL: formData.tbbModel,
+    THEORICALCHARGE: formData.theoricalCharge,
+    MACHINECAPACITY: formData.machineCapacity,
+    PlcModel: formData.plcModel,
+    IP: formData.ip,
     PORT: 8080,
-    VERSION: machine.version,
-    NOZZLECOUNT: machine.nozzleCount,
-    theoricalChargeDuration: machine.theoricalChargeDuration,
-    REELCOUNT: machine.reelCount,
-    STEAMUNIT: machine.steamUnit,
-    INUSE: machine.inUse,
-    MTTEMPIO: machine.MTTempIo,
-    STEAMKGPERHOUR: machine.steamKgPerHour,
-    STEAMVALVEDO: machine.steamValveDo,
-    ADDITIONALTANK1: machine.additionalTank1,
-    ADDITIONALTANK2: machine.additionalTank2,
-    ADDITIONALTANK3: machine.additionalTank3,
-    ADDITIONALTANK4: machine.additionalTank4,
-    RESERVETANK: machine.reserveTank,
-    STOREELECTRICITYASINC: machine.storeElectricityAsInc,
-    THEORETICALWATER: machine.theoreticalWater,
-    THEORETICALSTEAM: machine.theoreticalSteam,
+    VERSION: formData.version,
+    NOZZLECOUNT: formData.nozzleCount,
+    theoricalChargeDuration: formData.theoricalChargeDuration,
+    REELCOUNT: formData.reelCount,
+    STEAMUNIT: formData.steamUnit,
+    INUSE: formData.inUse,
+    MTTEMPIO: formData.MTTempIo,
+    STEAMKGPERHOUR: formData.steamKgPerHour,
+    STEAMVALVEDO: formData.steamValveDo,
+    ADDITIONALTANK1: formData.additionalTank1,
+    ADDITIONALTANK2: formData.additionalTank2,
+    ADDITIONALTANK3: formData.additionalTank3,
+    ADDITIONALTANK4: formData.additionalTank4,
+    RESERVETANK: formData.reserveTank,
+    STOREELECTRICITYASINC: formData.storeElectricityAsInc,
+    THEORETICALWATER: formData.theoreticalWater,
+    THEORETICALSTEAM: formData.theoreticalSteam,
   })
-
   return res
 })
