cbbb7c92ad569f55c88a7164cfeac657f13364db
mucahityaman <mucahit.yaman@eliarge.com>
2025-08-21 12:36:22 +0300

    refactor(PE): apply guard clause to machineId endpoint

--- apps/program-editor/server/api/machine/[machine_id].ts
+++ apps/program-editor/server/api/machine/[machine_id].ts
@@ -1,12 +1,22 @@
 import { inferBoolean } from '@teleskop/utils'
 import { MachineController } from '~/server/classes/MachineController'
+import { PError } from '~/server/error'
 import logger from '~/server/logger'
 
 export default defineAuthEventHandler(async (event) => {
   const { machine_id } = getRouterParams(event)
+  const machineId = Number(machine_id)
+
+  if (!Number.isInteger(machineId)) {
+    throw new PError('INVALID_MACHINE_OR_PROGRAM_NUMBER', { machineId, programNo: 0 })
+  }
+
   const query = getQuery(event)
-  const machineId = Number.parseInt(machine_id)
   const machine = await MachineController.create(machineId)
+  if (!machine) {
+    throw new PError('MACHINE_NOT_FOUND', { machineId })
+  }
+
   const editable = inferBoolean(query.editable as string, false)
   const machineInfo = await machine.getMachineInfo(editable)
   logger.info(`User: ${event.context.kauth?.name}. Fetching machine ${machineId}.`)
