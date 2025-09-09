efb963c6858d02ca39875db3c0f65d52c720287e
Ege İliklier <ege.iliklier@eliarge.com>
2025-09-09 20:38:42 +0300

    chore(project-translations): add `TopbarProjectLocaleSelect` component

--- packages/project-translations/nuxt.config.ts
+++ packages/project-translations/nuxt.config.ts
@@ -8,4 +8,12 @@ export default defineNuxtConfig({
     teleskopDatabase: '',
     teleskopInstanceName: '',
   },
+  i18n: {
+    langDir: './locales',
+    locales: [
+      { code: 'en-GB', file: './en.json' },
+      { code: 'tr', file: './tr.json' },
+      { code: 'pt', file: './pt.json' },
+    ],
+  },
 })
