1bf9193f12c63e7ef06967860fadf83f5d000c49
mucahityaman <mucahit.yaman@eliarge.com>
2025-09-24 15:38:10 +0300

    chore(migration-service): update migration files

--- apps/migration-service/src/migrations/105_parameter-group.ts
+++ apps/migration-service/src/migrations/105_parameter-group.ts
@@ -2,20 +2,34 @@
 import type { Knex } from 'knex'
 
 export async function up(knex: Knex) {
-  const columnExists = await knex.schema.hasColumn('BFCOMMANDPARAMETERS', 'PARAMETERGROUP')
+  const bfColumnExists = await knex.schema.hasColumn('BFCOMMANDPARAMETERS', 'PARAMETERGROUP')
+  const baColumnExists = await knex.schema.hasColumn('BACOMMANDPARAMETERS', 'PARAMETERGROUP')
 
-  if (!columnExists) {
+  if (!bfColumnExists) {
     await knex.schema.alterTable('BFCOMMANDPARAMETERS', (table) => {
+      table.integer('PARAMETERGROUP').nullable()
+    })
+  }
+
+  if (!baColumnExists) {
+    await knex.schema.alterTable('BACOMMANDPARAMETERS', (table) => {
       table.integer('PARAMETERGROUP').nullable()
     })
   }
 }
 
 export async function down(knex: Knex) {
-  const columnExists = await knex.schema.hasColumn('BFCOMMANDPARAMETERS', 'PARAMETERGROUP')
+  const bfColumnExists = await knex.schema.hasColumn('BFCOMMANDPARAMETERS', 'PARAMETERGROUP')
+  const baColumnExists = await knex.schema.hasColumn('BACOMMANDPARAMETERS', 'PARAMETERGROUP')
 
-  if (columnExists) {
+  if (bfColumnExists) {
     await knex.schema.alterTable('BFCOMMANDPARAMETERS', (table) => {
+      table.dropColumn('PARAMETERGROUP')
+    })
+  }
+
+  if (baColumnExists) {
+    await knex.schema.alterTable('BACOMMANDPARAMETERS', (table) => {
       table.dropColumn('PARAMETERGROUP')
     })
   }
