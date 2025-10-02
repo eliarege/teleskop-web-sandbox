46fad49816d8e34e115c9f955fba6a5a3763048b
Ege İliklier <ege.iliklier@eliarge.com>
2025-10-02 21:35:44 +0300

    feat(tbb-ftp-client): support new format of command parameters
    [PK-413](https://eliarelektronik.atlassian.net/issues/?jql=project%20%3D%20PK&selectedIssue=PK-413&wildcardFlag=true)

--- packages/tbb-ftp-client/src/parsers/parseCommandParams.ts
+++ packages/tbb-ftp-client/src/parsers/parseCommandParams.ts
@@ -1,42 +1,78 @@
-const pattern = /^(\d+) (\d+) "([^"]*)" "([^"]*)" "([^"]*)" (\d+) (-?\d+(?:\.\d+)?) (-?\d+(?:\.\d+)?) (-?\d+(?:\.\d+)?) (\d+)(?: (.+))?$/gim
+import type { CommandParameter } from '../types'
 
-// TODO: ["Calisma","0","Calis","1"] => [{name: "calis", value: 0}, ...]
+// 1: Float, 2: Integer, 3: String, 4: List
+const pattern = /(-?\d+\.\d+)|(-?\d+)|("[^"]*")|(\[[^\]]*\])/g
+
 /**
  * **Path**: `/tbb6500/data/commands/params`
  *
  * **Example**:
- * ```txt
- * 8 0 "SP 12" "Mikser Off Delay" "" 3 30 0 600 2`
- * 9 0 "SP 13" "Mikser" "" 1 0 0 1 0 ["Calisma","0","Calis","1"]
- * 10 0 "SP 2" "Litre" "IK1 Ust Seviye*IK1 Oran/100" 3 0 0 1000 0
+ * ```sh
+ * # Eski format, seçim listesiz
+ * 1 0 "SP 1" "Süre" "" 3 300 0 7200 2
+ * # Eski format seçim listeli
+ * 2 0 "SP 2" "Dara" "" 3 0 0 1 0 ["Dara Alma","0","Dara Al","1"]
+ * # Yeni format, seçim listesiz, makine sabiti seçimleri yapılmış
+ * 3 0 "SP 3" "Süre" "" 3 300 0 7200 2 12 34
+ * # Yeni format, seçim listeli, makine sabiti seçimleri yapılmış
+ * 1 0 "SP 4" "Dara" "" 3 0 0 1 0 ["Dara Alma","0","Dara Al","1"] 12 44
+ * # Yeni format, seçim listeli, makine sabiti seçimleri yapılmamış
+ * 2 0 "SP 5" "Artan Azalan" "IK1 Ust Seviye*IK1 Oran/100" 3 1 0 1 0 ["Azalan","0","Artan","1"] -1 -1
+ * # Yeni format, seçim listesiz, makine sabiti seçimleri yapılmamış
+ * 3 0 "SP 6" "Alarm Toleransı" "" 3 0 0 100 0 -1 -1
  * ```
  */
+
 export function parseCommandParams(content: string) {
-  const groups = []
-  let match = pattern.exec(content)
-  while (match !== null) {
-    const group = {
-      commandNo: Number.parseInt(match[1]),
-      name: match[3],
-      paramName: match[4],
-      paramFormula: match[5],
-      binding: Number.parseInt(match[6]),
-      defaultValue: Number.parseFloat(match[7]),
-      minValue: Number.parseFloat(match[8]),
-      maxValue: Number.parseFloat(match[9]),
-      graphic: Number.parseInt(match[10]),
-      selectionList: match[11] ? processSelectionList(JSON.parse(match[11])) : null,
+  const parameters: CommandParameter[] = []
+  const lines = content.split('\n').map(l => l.trim()).filter(Boolean)
+
+  for (const line of lines) {
+    const parts = [] as any[] // number | string | { name: string, value: number }[]
+    let match = pattern.exec(line)
+    let hasSelectionList = false
+    while (match !== null) {
+      if (match[1]) {
+        parts.push(Number.parseFloat(match[1]))
+      } else if (match[2]) {
+        parts.push(Number.parseInt(match[2]))
+      } else if (match[3]) {
+        parts.push(match[3].slice(1, -1))
+      } else if (match[4]) {
+        parts.push(processSelectionList(JSON.parse(match[4])))
+        hasSelectionList = true
+      }
+      match = pattern.exec(line)
     }
-    groups.push(group)
-    match = pattern.exec(content)
+    const parameter: CommandParameter = {
+      commandNo: parts[0],
+      name: parts[2],
+      paramName: parts[3],
+      paramFormula: parts[4],
+      binding: parts[5],
+      defaultValue: parts[6],
+      minValue: parts[7],
+      maxValue: parts[8],
+      graphic: parts[9],
+      selectionList: hasSelectionList ? parts[10] : null,
+    }
+    if (parts.length > 10 + Number(hasSelectionList)) {
+      parameter.machineConstantIdMin = parts[10 + Number(hasSelectionList)]
+      parameter.machineConstantIdMax = parts[11 + Number(hasSelectionList)]
+    }
+
+    parameters.push(parameter)
   }
-  return groups
+  return parameters
 }
 
 function processSelectionList(list: string[]) {
   const processedList: { name: string, value: number }[] = []
   for (let i = 0; i < list.length; i += 2) {
-    processedList.push({ name: list[i], value: Number(list[i + 1]) })
+    processedList.push({
+      name: list[i],
+      value: Number(list[i + 1]),
+    })
   }
   return processedList
 }
