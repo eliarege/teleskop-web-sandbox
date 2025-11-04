51d350dd717685d8aa6102286d1fad4cdd3ca22e
Ege İliklier <ege.iliklier@eliarge.com>
2025-11-04 16:14:18 +0300

    refactor(PE): remove caching logic from machineStore.get method

--- apps/program-editor/server/classes/MachineStore.ts
+++ apps/program-editor/server/classes/MachineStore.ts
@@ -3,19 +3,13 @@ import { MachineController } from './MachineController'
 import { T7ProgramClient, TonelloProgramClient } from './ProgramClient'
 
 class MachineStore {
-  readonly cache = new Map()
   async get(id: number): Promise<MachineController> {
-    if (this.cache.has(id)) {
-      return this.cache.get(id)
-    }
-
     const machine = await fetchMachineDetails(id)
     const client = machine.tbbModel === 'Tonello'
       ? new TonelloProgramClient(id, machine.host)
       : new T7ProgramClient(id, machine.host)
 
     const controller = new MachineController(id, client)
-    this.cache.set(id, controller)
     return controller
   }
 }
