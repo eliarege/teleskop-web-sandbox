ccade4cb678f780dd8be5fac45d1d1917fb29720
Onur Enginer <onur.enginer@eliarge.com>
2025-11-07 16:48:39 +0300

    feat(recipes): JobOrderOverview access from JorOrderList added

--- apps/recipes/pages/jobOrders/print.vue
+++ apps/recipes/pages/jobOrders/print.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
-const jobOrderMaterials = ref(JSON.parse(sessionStorage.getItem('jobOrderMaterials')))
-const jobOrderParams = ref(JSON.parse(sessionStorage.getItem('jobOrderParams')))
-const jobOrderMachines = ref(JSON.parse(sessionStorage.getItem('jobOrderMachines')))
-const jobOrderRecipeParams = ref(JSON.parse(sessionStorage.getItem('jobOrderRecipeParams')))
+const jobOrderMaterials = ref(JSON.parse(sessionStorage.getItem('jobOrderMaterials') || 'null'))
+const jobOrderParams = ref(JSON.parse(sessionStorage.getItem('jobOrderParams') || 'null'))
+const jobOrderMachines = ref(JSON.parse(sessionStorage.getItem('jobOrderMachines') || 'null'))
+const jobOrderRecipeParams = ref(JSON.parse(sessionStorage.getItem('jobOrderRecipeParams') || 'null'))
+const jobOrderBatchNo = ref(JSON.parse(sessionStorage.getItem('jobOrderBatchNo') || 'null'))
+
+sessionStorage.removeItem('jobOrderMaterials')
+sessionStorage.removeItem('jobOrderParams')
+sessionStorage.removeItem('jobOrderMachines')
+sessionStorage.removeItem('jobOrderRecipeParams')
+sessionStorage.removeItem('jobOrderBatchNo')
 </script>
 
 <template>
   <JobOrderOverview
+    v-if="jobOrderMaterials"
     :steps="jobOrderMaterials"
     :params="jobOrderParams"
     :machines="jobOrderMachines"
     :recipe-params="jobOrderRecipeParams"
+  />
+  <JobOrderOverview
+    v-else-if="jobOrderBatchNo"
+    :batch-no="jobOrderBatchNo"
   />
 </template>
