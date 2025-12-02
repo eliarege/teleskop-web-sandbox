b34f1aa518efa4ea3b9343493b9e74e96d88bee8
mucahityaman <mucahit.yaman@eliarge.com>
2025-12-02 22:32:00 +0300

    fix(PE): add goRoot computed property to properly handle root navigation logic
    On pages where the default page is not an empty path, home navigation failed
    to redirect to the correct root path. This commit introduces a prop
    to TopbarHomeButton to allow consumers to specify if the button should navigate to the root path.

--- packages/nuxt-base/components/topbar/TopbarHomeButton.vue
+++ packages/nuxt-base/components/topbar/TopbarHomeButton.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+const props = defineProps<{
+  goRoot?: boolean
+}>()
 const route = useRoute()
 const appList = useAppList()
 const rootUrl = appList.find(a => a.name === 'root')?.url
 
 const homeUrl = computed(() => {
-  if (route.path === '/' && rootUrl) {
+  if ((props.goRoot || route.path === '/') && rootUrl) {
     return { to: rootUrl, external: true }
   } else {
     return { to: '/', external: false }
