ab5333ad4c62a0991fb680881bd5331ffa5e8cb0
Ege İliklier <ege.iliklier@eliarge.com>
2025-12-19 16:59:05 +0300

    chore(nuxt-base): enable server-side translations

--- packages/nuxt-base/nuxt.config.ts
+++ packages/nuxt-base/nuxt.config.ts
@@ -78,6 +78,9 @@ export default defineNuxtConfig({
       { code: 'tr', name: 'Türkçe', file: 'tr.json' },
       { code: 'pt', name: 'Português', file: 'pt.json' },
     ],
+    experimental: {
+      localeDetector: `./i18n.localeDetector.ts`,
+    },
   },
   vite: {
     build: {
