77e7f8385a33f2a441a678f0896017963cd66150
Ege İliklier <ege.iliklier@eliarge.com>
2026-05-19 10:32:57 +0300

    fix(RE): ensure it's a number in program query

--- apps/recipes/server/api/programs/templates/index.get.ts
+++ apps/recipes/server/api/programs/templates/index.get.ts
@@ -2,6 +2,19 @@ import { dmsDB } from '~/server/connectionPool'
 
 export default defineEventHandler(async (event) => {
   const { machineId } = getQuery(event)
+  if (!machineId) {
+    throw createError({
+      statusCode: 400,
+      message: 'machineId is required',
+    })
+  }
+  // check if number
+  if (isNaN(Number(machineId))) {
+    throw createError({
+      statusCode: 400,
+      message: 'machineId must be a number',
+    })
+  }
   const res = await dmsDB('PROGRAM_HEADER as p').select({
     programNo: 'p.program_no',
     programName: 'p.program_name',
@@ -10,7 +23,7 @@ export default defineEventHandler(async (event) => {
     dyeRequests: 'p.dye_requests',
     saltRequests: 'p.salt_requests',
   })
-    .where('p.machine_id', machineId)
+    .where('p.machine_id', Number(machineId))
     .leftJoin('PROGRAM_TEMPLATE as t', function () {
       this.on('p.program_no', 't.program_no')
         .andOn('p.machine_id', 't.machine_id')
