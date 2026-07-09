b9aa378af0ea637666122704b80ef6275754b8e8
yagiz erdem <yagizerdem819@gmail.com>
2026-07-09 17:12:32 +0300

    update

---
+++ act-test.bat
@@ -0,0 +1,13 @@
+@echo off
+
+FOR /F "tokens=*" %%i IN ('gh auth token') DO SET "TOKEN=%%i"
+
+act push ^
+  -W ./.github/workflows/teleskop-web-sandbox.yml ^
+  --eventpath ./.github/event.json ^
+  -s GITHUB_TOKEN=%TOKEN% ^
+  --env GITHUB_REF=refs/tags/v0.0.1 ^
+  --env GITHUB_REF_NAME=v0.0.1 ^
+  -P self-hosted=ghcr.io/catthehacker/ubuntu:act-latest ^
+  -P docker=ghcr.io/catthehacker/ubuntu:act-latest
+
